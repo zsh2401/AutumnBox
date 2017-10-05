f81a201fb998e18075ff3af67662d38b1dd29545
zsh2401 <zsh2401@163.com>
2017-10-06 06:38:40 +0800

    ...
    Former-commit-id: 1543242fef3a08af0e59ff2d8d6f183abf65e87e

---
+++ AutumnBox/Helper/HeaderTest.cs
@@ -0,0 +1,33 @@
+﻿/* =============================================================================*\
+* >>English
+*
+* Filename: HeaderTest
+* Description: 
+*
+* Created: 2017/10/6 6:38:07
+* Compiler: Visual Studio 2017
+*
+* Author: zsh2401
+=================================================================================
+* >>中文
+*
+* 文件名: HeaderTest
+* 描述: 
+*
+* 创建于: 2017/10/6 6:38:07
+* 编译器: Visual Studio 2017
+*
+* 编写者: zsh2401
+\* =============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.Helper
+{
+    public class HeaderTest
+    {
+    }
+}
