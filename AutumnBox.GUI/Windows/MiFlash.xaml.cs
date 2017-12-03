dbff11b1102f92f4a3d62f2bff8aca72d05ad26d
zsh2401 <zsh2401@163.com>
2017-12-03 21:34:06 +0800

    修复编译错误
    Former-commit-id: 39949bc7c25d795c1ac0283eda6fe9c1201241c5

--- AutumnBox.GUI/Windows/MiFlash.xaml.cs
+++ AutumnBox.GUI/Windows/MiFlash.xaml.cs
@@ -20,130 +20,131 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace AutumnBox.GUI.Windows { 
-//{
-//    /// <summary>
-//    /// MiFlash.xaml 的交互逻辑
-//    /// </summary>
-//    public partial class MiFlash : Window
-//    {
-//        private class Timer
-//        {
-//            private Thread thread;
-//            private System.Windows.Controls.Label timeLabel;
-//            public void Start(System.Windows.Controls.Label timeLabel)
-//            {
-//                this.timeLabel = timeLabel;
-//                thread = new Thread(_Main);
-//                thread.Start();
-//            }
-//            private void _Main()
-//            {
-//                int sec = 0;
-//                while (true)
-//                {
-//                    Thread.Sleep(1000);
-//                    sec++;
-//                    timeLabel.Dispatcher.Invoke(() =>
-//                    {
-//                        timeLabel.Content = $"{sec} S";
-//                    });
-//                }
-//            }
-//            public void Stop()
-//            {
-//                thread.Abort();
+namespace AutumnBox.GUI.Windows
+{
+    /// <summary>
+    /// MiFlash.xaml 的交互逻辑
+    /// </summary>
+    public partial class MiFlash : Window
+    {
+        private class Timer
+        {
+            private Thread thread;
+            private System.Windows.Controls.Label timeLabel;
+            public void Start(System.Windows.Controls.Label timeLabel)
+            {
+                this.timeLabel = timeLabel;
+                thread = new Thread(_Main);
+                thread.Start();
+            }
+            private void _Main()
+            {
+                int sec = 0;
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    sec++;
+                    timeLabel.Dispatcher.Invoke(() =>
+                    {
+                        timeLabel.Content = $"{sec} S";
+                    });
+                }
+            }
+            public void Stop()
+            {
+                thread.Abort();
 
-//            }
-//        }
-//        private Timer timer = new Timer();
-//        private FunctionModuleProxy Fmp = null;
-//        public MiFlash()
-//        {
-//            InitializeComponent();
-//            TitleBar.OwnerWindow = this;
-//        }
-//        private void buttonStartOrCancel_Click(object sender, RoutedEventArgs e)
-//        {
-//            if (buttonStartOrCancel.Content == App.Current.Resources["btnCancel"])
-//            {
-//                Stop();
-//                buttonStartOrCancel.Content = App.Current.Resources["btnStart"];
-//                return;
-//            }
-//            else
-//            {
-//                if (Start())
-//                {
-//                    buttonStartOrCancel.Content = App.Current.Resources["btnCancel"];
-//                }
-//            }
-//        }
-//        private bool Start()
-//        {
-//            if (TextBoxPath.Text == App.Current.Resources["PleaseSelectFloderPackageWireFlash"].ToString())
-//            {
-//                MMessageBox.FastShow(App.Current.MainWindow, App.Current.Resources["Warning"].ToString(), App.Current.Resources["PleaseSelectFloderPackageWireFlash"].ToString());
-//                return false;
-//            }
-//            Fmp = FunctionModuleProxy.Create(typeof(Basic.Function.Modules.MiFlash),
-//            new MiFlasherArgs(App.SelectedDevice)
-//            {
-//                FloderPath = TextBoxPath.Text,
-//                Type = GetFlashType(),
-//            });
-//            Fmp.OutputReceived += (s, _e) =>
-//            {
-//                this.Dispatcher.Invoke(() =>
-//                {
-//                    TextBoxOutput.AppendText("\n" + _e.Text);
-//                    LabelNow.Foreground = _e.IsError ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Green);
-//                    LabelNow.Content = _e.Text;
-//                    TextBoxOutput.ScrollToEnd();
-//                });
-//            };
-//            Fmp.Finished += (s, e) =>
-//            {
-//                this.Dispatcher.Invoke(() =>
-//                {
-//                    ModuleResultWindow.FastShow(e.Result);
-//                });
-//            };
-//            Fmp.AsyncRun();
-//            timer.Start(this.LabelNow);
-//            return true;
-//        }
-//        private void Stop()
-//        {
-//            Fmp.ForceStop();
-//            timer.Stop();
-//        }
-//        private MiFlashType GetFlashType()
-//        {
-//            if (RBFlashAllExceptStorage.IsChecked == true) return MiFlashType.FlashAllExceptStorage;
-//            else if (RBFlashAllExceptStorageData.IsChecked == true) return MiFlashType.FlashAllExceptStorageAndData;
-//            else return MiFlashType.FlashAll;
-//        }
-//        private void buttonSelectFloder_Click(object sender, RoutedEventArgs e)
-//        {
-//            FolderBrowserDialog fbd = new FolderBrowserDialog();
-//            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-//            {
-//                if (Check(fbd.SelectedPath))
-//                {
-//                    TextBoxPath.Text = fbd.SelectedPath;
-//                }
-//                else
-//                {
-//                    MMessageBox.FastShow(App.Current.MainWindow, App.Current.Resources["Warning"].ToString(), App.Current.Resources["msgMiFlashFloderError"].ToString());
-//                }
-//            }
-//        }
-//        private static bool Check(string path)
-//        {
-//            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_bat} "))) return false;
-//            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_except_storage_bat}"))) return false;
-//            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_except_storage_bat}"))) return false;
-//            return true;
-//        }
+            }
+        }
+        private Timer timer = new Timer();
+        private FunctionModuleProxy Fmp = null;
+        public MiFlash()
+        {
+            InitializeComponent();
+            TitleBar.OwnerWindow = this;
+        }
+        private void buttonStartOrCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonStartOrCancel.Content == App.Current.Resources["btnCancel"])
+            {
+                Stop();
+                buttonStartOrCancel.Content = App.Current.Resources["btnStart"];
+                return;
+            }
+            else
+            {
+                if (Start())
+                {
+                    buttonStartOrCancel.Content = App.Current.Resources["btnCancel"];
+                }
+            }
+        }
+        private bool Start()
+        {
+            if (TextBoxPath.Text == App.Current.Resources["PleaseSelectFloderPackageWireFlash"].ToString())
+            {
+                MMessageBox.FastShow(App.Current.MainWindow, App.Current.Resources["Warning"].ToString(), App.Current.Resources["PleaseSelectFloderPackageWireFlash"].ToString());
+                return false;
+            }
+            Fmp = FunctionModuleProxy.Create(typeof(Basic.Function.Modules.MiFlash),
+            new MiFlasherArgs(App.SelectedDevice)
+            {
+                FloderPath = TextBoxPath.Text,
+                Type = GetFlashType(),
+            });
+            Fmp.OutputReceived += (s, _e) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    TextBoxOutput.AppendText("\n" + _e.Text);
+                    LabelNow.Foreground = _e.IsError ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Green);
+                    LabelNow.Content = _e.Text;
+                    TextBoxOutput.ScrollToEnd();
+                });
+            };
+            Fmp.Finished += (s, e) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    ModuleResultWindow.FastShow(e.Result);
+                });
+            };
+            Fmp.AsyncRun();
+            timer.Start(this.LabelNow);
+            return true;
+        }
+        private void Stop()
+        {
+            Fmp.ForceStop();
+            timer.Stop();
+        }
+        private MiFlashType GetFlashType()
+        {
+            if (RBFlashAllExceptStorage.IsChecked == true) return MiFlashType.FlashAllExceptStorage;
+            else if (RBFlashAllExceptStorageData.IsChecked == true) return MiFlashType.FlashAllExceptStorageAndData;
+            else return MiFlashType.FlashAll;
+        }
+        private void buttonSelectFloder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (Check(fbd.SelectedPath))
+                {
+                    TextBoxPath.Text = fbd.SelectedPath;
+                }
+                else
+                {
+                    MMessageBox.FastShow(App.Current.MainWindow, App.Current.Resources["Warning"].ToString(), App.Current.Resources["msgMiFlashFloderError"].ToString());
+                }
+            }
+        }
+        private static bool Check(string path)
+        {
+            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_bat} "))) return false;
+            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_except_storage_bat}"))) return false;
+            if (!(File.Exists($"{path}/{Basic.Function.Modules.MiFlash.flash_all_except_storage_bat}"))) return false;
+            return true;
+        }
+    }
 }
