58bf1c1baa5a8744cfb28b9e7fd6326621e7ed80
zsh2401 <zsh2401@163.com>
2017-12-22 02:51:21 +0800

    ..
    Former-commit-id: f319e1a97e99fa6f7efb90b1a729c092a6865b6e

--- AutumnBox.ConsoleTester/Program.cs
+++ AutumnBox.ConsoleTester/Program.cs
@@ -3,6 +3,8 @@ using AutumnBox.Basic.Devices;
 using AutumnBox.Basic.Executer;
 using AutumnBox.Support;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -28,16 +30,22 @@ namespace AutumnBox.ConsoleTester
         };
         unsafe static void Main(string[] args)
         {
+            //Process.Start("steam://open/console");
+            string[] lines  = File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+            string fullText = File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts");
+            StringBuilder sb = new StringBuilder(fullText);
+            sb.Append("23.52.74.146 steamcommunity.com");
+            File.WriteAllText(@"C:\Windows\System32\drivers\etc\hosts",sb.ToString());
             //var devices = new DevicesGetter().GetDevices();
             //devices.ForEach((i) =>
             //{
             //    Console.WriteLine(i.Serial.ToString());
             //});
-            var buildInfo = DeviceInfoHelper.GetBuildInfoWithSu(new Serial("192.168.0.12:5555"));
-            foreach(var en in buildInfo)
-            {
-                Console.WriteLine(en.Key,en.Value);
-            }
+            //var buildInfo = DeviceInfoHelper.GetBuildInfoWithSu(new Serial("192.168.0.12:5555"));
+            //foreach(var en in buildInfo)
+            //{
+            //    Console.WriteLine(en.Key,en.Value);
+            //}
             Console.ReadKey();
         }
         public static void WriteWithColor(Action a, ConsoleColor color = ConsoleColor.White)
