06dd2b2f0832d7b8fabcda714661ad107dc1d84a
zsh2401 <zsh2401@163.com>
2017-12-22 16:40:44 +0800

    update files
    Former-commit-id: f21d7487069c648ae779dbb0893901a5bbd679a3

--- AutumnBox.GUI/NetUtil/Urls.cs
+++ AutumnBox.GUI/NetUtil/Urls.cs
@@ -16,8 +16,8 @@ namespace AutumnBox.GUI.NetUtil
     internal static class Urls
     {
         internal readonly static string HELP_PAGE = "https://github.com/zsh2401/AutumnBox/wiki/Chinese-Home";
-        internal readonly static string STATISTICS_API = "http://softsupport.alonet.top/autumnbox/report.php";
-        internal readonly static string MOTD_API = "http://autumnbox.zsh2401.top/__api/motd/";
-        internal readonly static string UPDATE_API = "http://autumnbox.zsh2401.top/__api/update/";
+        internal readonly static string STATISTICS_API = "http://api.zsh2401.top/autumnbox/report.php";
+        internal readonly static string MOTD_API = "http://autumnbox.zsh2401.top/motd.html";
+        internal readonly static string UPDATE_API = "http://autumnbox.zsh2401.top/update.html";
     }
 }
