bb2f053083c181d04ffc0b3e5ddc878fb1ff2f35
zsh2401 <zsh2401@163.com>
2018-01-15 03:22:29 +0800

    建立AutumnBox.Basic.Device.PackageManage命名空间,但里面的类均未完成
    Former-commit-id: bd6bed47d4216fda4ae95261b478bc333340dabd

---
+++ AutumnBox.Basic/Device/PackageManage/Package.cs
@@ -0,0 +1,38 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/1/15 3:16:25 (UTC +8:00)
+** desc： ...
+*************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.Basic.Device.PackageManage
+{
+    public class Package
+    {
+        public string Name { get; private set; }
+        public string MainActivity
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public int? Size
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+
+        public Package(string packageName)
+        {
+            this.Name = packageName;
+        }
+    }
+}
---
+++ AutumnBox.Basic/Device/PackageManage/PackageManagerShared.cs
@@ -0,0 +1,23 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/1/15 3:11:06 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.Basic.Executer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.Basic.Device.PackageManage
+{
+    public static class PackageManagerShared
+    {
+        internal static readonly CommandExecuter executer;
+        static PackageManagerShared()
+        {
+            executer = new CommandExecuter();
+        }
+    }
+}
