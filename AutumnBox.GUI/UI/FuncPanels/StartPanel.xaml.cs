bc6be36068c7463427e00431cd2c7e67e44ff61c
zsh2401 <zsh2401@163.com>
2018-02-21 19:04:19 +0800

    deploy start page
    Former-commit-id: 096032ce7cd30f4a3fcffa062e9859b18c316d7a

--- AutumnBox.GUI/UI/FuncPanels/StartPanel.xaml.cs
+++ AutumnBox.GUI/UI/FuncPanels/StartPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using AutumnBox.Support.CstmDebug;
+﻿using AutumnBox.GUI.NetUtil;
+using AutumnBox.Support.CstmDebug;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,27 +21,35 @@ namespace AutumnBox.GUI.UI.FuncPanels
     /// <summary>
     /// StartPanel.xaml 的交互逻辑
     /// </summary>
-    public partial class StartPanel : UserControl,ILogSender
+    public partial class StartPanel : UserControl, ILogSender
     {
         public StartPanel()
         {
             InitializeComponent();
-            //WebBrowser.Navigating += (s, e) =>
-            //{
-            //    Debug.WriteLine(e.WebRequest.RequestUri.ToString());
-            //    bool isInnerWeb = e.WebRequest.RequestUri.ToString().Contains("autumnbox_inner");
-            //    if (!isInnerWeb)
-            //    {
-            //        e.Cancel = true;
-            //        Process.Start(e.WebRequest.RequestUri.AbsoluteUri);
-            //    }
-            //};
-            WebBrowser.Navigate(new Uri("http://atmb.top"));
-
         }
 
         public string LogTag => "WB";
 
         public bool IsShowLog => true;
+
+        private void TBOfficialWebsite_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(Urls.OFFICIAL_WEBSITE);
+        }
+
+        private void WB_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            Debug.WriteLine("heheh?" + e.Uri);
+            if (!e.Uri.ToString().Contains("/api/start/"))
+            {
+                e.Cancel = true;
+                Process.Start(e.Uri.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WB.Refresh();
+        }
     }
 }
