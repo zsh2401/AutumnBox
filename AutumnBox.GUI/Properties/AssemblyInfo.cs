deb2e52511d7452a981113a068dedd968d7a2adf
zsh2401 <zsh2401@163.com>
2018-03-03 03:57:26 +0800

    v0.53;调整开始面板布局
    Former-commit-id: 536b4f8f3b8435a5b7351cc53339b7681aa071bb

--- AutumnBox.GUI/Properties/AssemblyInfo.cs
+++ AutumnBox.GUI/Properties/AssemblyInfo.cs
@@ -62,11 +62,11 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.52.5.*")]
-[assembly: AssemblyFileVersion("0.52.5")]
+[assembly: AssemblyVersion("0.53.0.*")]
+[assembly: AssemblyFileVersion("0.53.0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
 
 //自定义的程序集Attribute
 //确定当前程序的Log位置
 //编译日期
-[assembly: CompiledDate(2018, 3, 1)]
+[assembly: CompiledDate(2018, 3, 3)]
