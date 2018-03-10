22379ad59c4297e34c1aa7eb5af20817dbdf6560
zsh2401 <zsh2401@163.com>
2018-03-10 16:50:03 +0800

    删除一处不必要的引用

--- AutumnBox.Basic.Flows/MiFlash/LineFlashPackageParser.cs
+++ AutumnBox.Basic.Flows/MiFlash/LineFlashPackageParser.cs
@@ -6,12 +6,9 @@
 ** desc： ...
 *********************************************************************************/
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AutumnBox.Basic.Flows.MiFlash
 {
