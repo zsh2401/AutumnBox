68638816e7c88a81a557ec8c5787e1bb2d257c66
zsh2401 <zsh2401@163.com>
2018-04-05 19:28:26 +0800

    修改加载逻辑,添加注释

--- AutumnBox.GUI/Windows/IAppLoadingWindow.cs
+++ AutumnBox.GUI/Windows/IAppLoadingWindow.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutumnBox.GUI.Windows
+﻿namespace AutumnBox.GUI.Windows
 {
     internal interface IAppLoadingWindow
     {
