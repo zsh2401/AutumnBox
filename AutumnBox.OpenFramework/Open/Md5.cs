e1da4d87201124ebaa4fb433b4dcfca782c5bdd7
zsh2401 <zsh2401@163.com>
2018-04-07 11:51:37 +0800

    将MD5类改为静态类

--- AutumnBox.OpenFramework/Open/Md5.cs
+++ AutumnBox.OpenFramework/Open/Md5.cs
@@ -6,14 +6,14 @@ namespace AutumnBox.OpenFramework.Open
     /// <summary>
     /// Md5校验类
     /// </summary>
-    public class Md5
+    public static class Md5
     {
         /// <summary>
         /// 会返回一个Md5值
         /// </summary>
         /// <param name="_path">需要校验的文件路径</param>
         /// <returns></returns>
-        public string GetMd5(string _path)
+        public static string GetMd5(string _path)
         {
             var strResult = "no md5";
             var strHashData = "";
@@ -46,7 +46,7 @@ namespace AutumnBox.OpenFramework.Open
         /// <param name="_path">需要校验的文件路径</param>
         /// <param name="_md5">需要校验的文件md5值</param>
         /// <returns></returns>
-        public bool CheckMd5(string _path, string _md5)
+        public static bool CheckMd5(string _path, string _md5)
         {
             var _fmd5 = GetMd5(_path);
             if (_fmd5 == _md5 || _fmd5.ToLower() == _md5) return true;
