429de978215c916474658c1b89bd587786780821
zsh2401 <zsh2401@163.com>
2018-04-17 18:37:20 +0800

    PoweronFuncs中的IAdbActivatorUI内的代码拆分并且改名

---
+++ AutumnBox.GUI/UI/FuncPanels/PoweronFuncsUx/TipAttribute.cs
@@ -0,0 +1,25 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/4/17 18:25:28 (UTC +8:00)
+** desc： ...
+*************************************************/
+
+using AutumnBox.Basic.Device;
+using AutumnBox.GUI.Helper;
+
+namespace AutumnBox.GUI.UI.FuncPanels.PoweronFuncsUx
+{
+    public class TipAttribute : FuncsPrecheckAttribute
+    {
+        public readonly string MsgKey;
+        public TipAttribute(string msg)
+        {
+            this.MsgKey = msg;
+        }
+
+        public override bool Check(DeviceBasicInfo tragetDevice)
+        {
+            return BoxHelper.ShowChoiceDialog("titleMustRead", MsgKey).ToBool();
+        }
+    }
+}
