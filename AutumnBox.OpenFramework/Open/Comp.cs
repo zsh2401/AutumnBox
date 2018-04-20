e3d3783ef85ec3288112df5e45be896bd90d8cce
zsh2401 <zsh2401@163.com>
2018-04-20 11:35:27 +0800

    ...

--- AutumnBox.OpenFramework/Open/Comp.cs
+++ AutumnBox.OpenFramework/Open/Comp.cs
@@ -4,10 +4,6 @@
 ** desc： ...
 *************************************************/
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutumnBox.OpenFramework.Open
 {
