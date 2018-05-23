1b48c677c2ecf60153eca0619a96956c1cea324c
zsh2401 <zsh2401@163.com>
2018-05-23 22:29:25 +0800

    修复命名空间BUG

--- AutumnBox.GUI/PaidVersion/LoginPanel.xaml.cs
+++ AutumnBox.GUI/PaidVersion/LoginPanel.xaml.cs
@@ -1,20 +1,7 @@
-﻿using AutumnBox.GUI.PaidVersion;
-using AutumnBox.GUI.UI.Fp;
+﻿using AutumnBox.GUI.UI.Fp;
 using AutumnBox.Support.Log;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AutumnBox.GUI.PaidVersion
 {
