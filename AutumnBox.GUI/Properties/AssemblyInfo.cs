9cb1075c8f8527409bcea3d62c5bb0b43b267646
zsh2401 <zsh2401@163.com>
2018-06-16 13:41:18 +0800

    筹备1.0.0.0版本

--- AutumnBox.GUI/Properties/AssemblyInfo.cs
+++ AutumnBox.GUI/Properties/AssemblyInfo.cs
@@ -66,10 +66,10 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.80.0.*")]
-[assembly: AssemblyFileVersion("0.80.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
 
 //自定义的程序集Attribute
 //编译日期
-[assembly: CompiledDate(2018, 6, 1)]
+[assembly: CompiledDate(2018, 6, 16)]
