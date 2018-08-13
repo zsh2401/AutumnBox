ef7ca6553366d0169534c0e86b551531ff123dbc
zsh2401 <zsh2401@163.com>
2018-08-13 09:50:45 +0800

    实现Dpi修改,截图,解锁系统分区

---
+++ AutumnBox.CoreModules/Extensions/EDpiChanger.cs
@@ -0,0 +1,24 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/8/13 9:26:11 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.GUI.Windows;
+using AutumnBox.OpenFramework.Extension;
+
+namespace AutumnBox.CoreModules.Extensions
+{
+    [ExtName("无ROOT修改DPI")]
+    [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
+    public class EDpiChanger : AutumnBoxExtension
+    {
+        public override int Main()
+        {
+            App.RunOnUIThread(() =>
+            {
+                new DpiChangeWindow(TargetDevice) { Owner = App.GetMainWindow() }.ShowDialog();
+            });
+            return OK;
+        }
+    }
+}
