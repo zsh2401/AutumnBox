8148972c8b7d9229e76f721ce3ff4e60582b2a9a
zsh2401 <zsh2401@163.com>
2018-08-13 09:50:45 +0800

    实现Dpi修改,截图,解锁系统分区

---
+++ AutumnBox.CoreModules/Extensions/EScreenShoter.cs
@@ -0,0 +1,44 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/8/13 9:37:22 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.Basic.Flows;
+using AutumnBox.OpenFramework.Extension;
+using System.Windows.Forms;
+
+namespace AutumnBox.CoreModules.Extensions
+{
+    [ExtName("截图并保存到电脑")]
+    [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
+    public class EScreenShoter : AutumnBoxExtension
+    {
+        public override int Main()
+        {
+            DialogResult dialogResult = DialogResult.No;
+            string path = null;
+            App.RunOnUIThread(() =>
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                dialogResult = fbd.ShowDialog();
+                path = fbd.SelectedPath;
+            });
+
+            if (dialogResult == DialogResult.OK)
+            {
+                var shoter = new ScreenShoter();
+                shoter.Init(new ScreenShoterArgs()
+                {
+                    DevBasicInfo = TargetDevice,
+                    SavePath = path
+                });
+                App.ShowLoadingWindow();
+                var output = shoter.Run();
+                App.CloseLoadingWindow();
+                Logger.Info(output.OutputData.ToString());
+                return OK;
+            }
+            return ERR;
+        }
+    }
+}
---
+++ AutumnBox.CoreModules/Extensions/EUnlockSystemParation.cs
@@ -0,0 +1,31 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/8/13 9:33:21 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.Basic.Device;
+using AutumnBox.Basic.FlowFramework;
+using AutumnBox.Basic.Flows;
+using AutumnBox.OpenFramework.Extension;
+
+namespace AutumnBox.CoreModules.Extensions
+{
+    [ExtName("解锁系统分区")]
+    [ExtRequiredDeviceStates(DeviceState.Poweron)]
+    [ExtRequireRoot]
+    public class EUnlockSystemParation : AutumnBoxExtension
+    {
+        public override int Main()
+        {
+            App.ShowLoadingWindow();
+            var unlocker = new SystemPartitionUnlocker();
+            unlocker.Init(new FlowArgs()
+            {
+                DevBasicInfo = TargetDevice
+            });
+            unlocker.Run();
+            App.CloseLoadingWindow();
+            return OK;
+        }
+    }
+}
