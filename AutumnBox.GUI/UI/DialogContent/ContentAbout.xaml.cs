ffe36c95c95b60702713613254a2e975c2773a89
zsh2401 <zsh2401@163.com>
2018-08-14 19:40:29 +0800

    使用MD框架,更进一步MD设计!

---
+++ AutumnBox.GUI/UI/DialogContent/ContentAbout.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AutumnBox.GUI.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutumnBox.GUI.UI.DialogContent
+{
+    /// <summary>
+    /// ContentAbout.xaml 的交互逻辑
+    /// </summary>
+    public partial class ContentAbout : UserControl
+    {
+        public ContentAbout()
+        {
+            InitializeComponent();
+            LabelVersion.Content = SystemHelper.CurrentVersion.ToString();
+#if !DEBUG
+            LabelVersion.Content += "-release";
+#else
+            LabelVersion.Content += "-debug";
+#endif
+            LabelCompiledDate.Content = SystemHelper.CompiledDate.ToString("MM-dd-yyyy");
+        }
+    }
+}
