c09520885d0c2e7ab171c5fd47295387b74805a6
zsh2401 <zsh2401@163.com>
2018-08-14 20:23:15 +0800

    重新实现部分设置界面功能

--- AutumnBox.GUI/UI/DialogContent/ContentSettings.xaml.cs
+++ AutumnBox.GUI/UI/DialogContent/ContentSettings.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutumnBox.GUI.Helper;
+using AutumnBox.GUI.I18N;
+using AutumnBox.GUI.Properties;
+using AutumnBox.GUI.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +27,32 @@ namespace AutumnBox.GUI.UI.DialogContent
         public ContentSettings()
         {
             InitializeComponent();
+            TGLaunchDebugNext.IsChecked = Settings.Default.ShowDebuggingWindowNextLaunch;
+
+            CBLanguage.ItemsSource = LanguageHelper.Langs;
+            CBLanguage.SelectedIndex = LanguageHelper.FindIndex(App.Current.Resources["LanguageCode"].ToString());
+            CBLanguage.SelectionChanged += CBLanguage_SelectionChanged;
+        }
+
+        private void CBLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LanguageHelper.SetLanguage((Language)CBLanguage.SelectedItem);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new DebugWindow().Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SystemHelper.CreateShortcutOnDesktop("AutumnBox", Environment.CurrentDirectory + "/AutumnBox.GUI.exe", "The AutumnBox-Dream of us");
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.ShowDebuggingWindowNextLaunch = TGLaunchDebugNext.IsChecked == true;
+            Settings.Default.Save();
         }
     }
 }
