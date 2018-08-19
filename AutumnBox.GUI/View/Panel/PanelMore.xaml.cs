a587a48bc4fbaac7b637b7f42a290bd2f2ca1136
zsh2401 <zsh2401@163.com>
2018-08-20 04:02:08 +0800

    初步完成"更多页面"的设计

--- AutumnBox.GUI/View/Panel/PanelMore.xaml.cs
+++ AutumnBox.GUI/View/Panel/PanelMore.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutumnBox.GUI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace AutumnBox.GUI.View.Panel
         public PanelMore()
         {
             InitializeComponent();
+            DataContext = new VMMore();
         }
     }
 }
---
+++ AutumnBox.GUI/ViewModel/VMMore.cs
@@ -0,0 +1,41 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/8/20 3:29:09 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.GUI.MVVM;
+using AutumnBox.OpenFramework;
+using AutumnBox.OpenFramework.Management;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.GUI.ViewModel
+{
+    class VMMore : ViewModelBase
+    {
+        public string ApiVersion { get; set; }
+        public FlexiableCommand OpenExtFloder { get; set; }
+        public FlexiableCommand OpenUrl { get; set; }
+        public VMMore()
+        {
+            OpenExtFloder = new FlexiableCommand(() =>
+            {
+                Process.Start(Manager.InternalManager.ExtensionPath);
+            });
+            OpenUrl = new FlexiableCommand((para) =>
+            {
+                try
+                {
+                    Process.Start(para.ToString());
+                }
+                catch { }
+            });
+            ApiVersion = BuildInfo.SDK_VERSION.ToString();
+        }
+
+    }
+}
