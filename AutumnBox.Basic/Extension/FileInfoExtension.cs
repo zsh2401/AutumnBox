cea69e2b158d2529ead2e523723a6079890b7671
Shunhong Zhang <zsh2401@163.com>
2018-09-06 11:25:18 +0800

    fix bugs

--- AutumnBox.Basic/Extension/FileInfoExtension.cs
+++ AutumnBox.Basic/Extension/FileInfoExtension.cs
@@ -24,14 +24,21 @@ namespace AutumnBox.Basic.Extension
         /// <param name="path"></param>
         public static void PushTo(this FileInfo fileInfo, IDevice device, string path)
         {
-            ThrowIf.IsNullArg(fileInfo);
-            ThrowIf.IsNullArg(path);
-            var builder = new AdbCommandBuilder();
-            builder.Device(device)
-                .Arg("push")
-                .ArgWithDoubleQuotation(fileInfo.FullName)
-                .ArgWithDoubleQuotation(path);
-            using (var command = builder.ToCommand())
+            if (fileInfo == null)
+            {
+                throw new ArgumentNullException(nameof(fileInfo));
+            }
+
+            if (device == null)
+            {
+                throw new ArgumentNullException(nameof(device));
+            }
+
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+            using (var command = new AdbCommand(device, $"push \"{fileInfo.FullName}\" \"{path}\""))
             {
                 var result = command.Execute();
                 if (result.ExitCode != 0)
@@ -47,15 +54,21 @@ namespace AutumnBox.Basic.Extension
         /// <param name="device"></param>
         public static void InstallTo(this FileInfo apkFileInfo, IDevice device)
         {
+            if (apkFileInfo == null)
+            {
+                throw new ArgumentNullException(nameof(apkFileInfo));
+            }
+
+            if (device == null)
+            {
+                throw new ArgumentNullException(nameof(device));
+            }
+
             if (apkFileInfo.Extension != ".apk")
             {
                 throw new ArgumentException("Is not apk file!", nameof(apkFileInfo));
             }
-            var builder = new AdbCommandBuilder();
-            builder.Device(device)
-                .Arg("install")
-                .ArgWithDoubleQuotation(apkFileInfo.FullName);
-            using (var command = builder.ToCommand())
+            using (var command = new AdbCommand(device, $"install \"{apkFileInfo.FullName}\""))
             {
                 var result = command.Execute();
                 if (result.ExitCode != 0)
