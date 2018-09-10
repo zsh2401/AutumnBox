2e07e405663fd2c77620307d747cdf29ef7fcfca
zsh2401 <zsh2401@163.com>
2018-09-10 17:24:21 +0800

    AutumnBox.Basic AutumnBox.OpenFx基本可用

--- AutumnBox.CoreModules/Lib/GodPower.cs
+++ AutumnBox.CoreModules/Lib/GodPower.cs
@@ -1,4 +1,7 @@
-﻿using AutumnBox.Basic.Data;
+﻿using AutumnBox.Basic.Calling.Adb;
+using AutumnBox.Basic.Data;
+using AutumnBox.Basic.Device;
+using AutumnBox.Basic.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,49 +10,42 @@ using System.Threading.Tasks;
 
 namespace AutumnBox.CoreModules.Lib
 {
-    public class GodPower : IDisposable,INotifyOutput
+    public class GodPower : DependOnDeviceObject
     {
-        public void Push() { }
-        public void RemoveAllUser() { }
-        public void RemoveAllAccount() { }
-        public void SetIceBox() { }
-        public void SetStopApp() { }
-        #region IDisposable Support
-        private bool disposedValue = false; // 要检测冗余调用
-
-        public event OutputReceivedEventHandler OutputReceived;
-
-        protected virtual void Dispose(bool disposing)
+        private const string GODPOWER_APK_INNER_RES = "Res.godpower.apk";
+        public ShellCommand GetRemoveUserCommand() { }
+        public ShellCommand GetRemoveAccountCommnad() { }
+        public ShellCommand GetSetIceBoxCommand() { }
+        public ShellCommand GetSetStopAppCommand() { }
+        public AdbCommand GetPushCommand()
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: 释放托管状态(托管对象)。
-                }
 
-                // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
-                // TODO: 将大型字段设置为 null。
-
-                disposedValue = true;
-            }
+        }
+        public void Push()
+        {
+            GetPushCommand().Execute().ThrowIfExitCodeNotEqualsZero();
         }
 
-        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
-        // ~GodPower() {
-        //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-        //   Dispose(false);
-        // }
 
-        // 添加此代码以正确实现可处置模式。
-        public void Dispose()
+        public void RemoveAllUser()
         {
-            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
-            Dispose(true);
-            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
 
+            GetRemoveUserCommand().Execute().ThrowIfExitCodeNotEqualsZero();
+        }
+        public void RemoveAllAccount()
+        {
+            GetRemoveAccountCommnad().Execute().ThrowIfExitCodeNotEqualsZero();
+        }
+        public void SetIceBox()
+        {
+            GetSetIceBoxCommand().Execute().ThrowIfExitCodeNotEqualsZero();
+        }
+        public void SetStopApp()
+        {
+            GetSetStopAppCommand();
+        }
+        public GodPower(IDevice device) : base(device)
+        {
+        }
     }
 }
