e341c1967b618d22240b9c3999f3f1f97ec1d863
zsh2401 <zsh2401@163.com>
2018-09-15 17:28:32 +0800

    新的适用于AutumnBox.Basic的log机制

---
+++ AutumnBox.Basic/Debugging/LoggingStation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Debugging
+{
+    public class LogEventArgs : EventArgs
+    {
+        public string Tag { get; set; } = "Unknown class";
+        public string Text { get; set; } = null;
+        public LogLevel Level { get; set; } = LogLevel.Info;
+        public LogEventArgs(string text)
+        {
+            this.Text = text;
+        }
+    }
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Fatal
+    }
+    public static class LoggingStation
+    {
+        public static event EventHandler<LogEventArgs> Logging;
+        internal static void RaiseEvent(object senderOrTag, object content, LogLevel level = LogLevel.Info)
+        {
+            var args = new LogEventArgs(content.ToString())
+            {
+                Tag = senderOrTag?.ToString() ?? "UnknownClass",
+                Level = level,
+            };
+            Logging?.Invoke(senderOrTag, args);
+        }
+    }
+}
