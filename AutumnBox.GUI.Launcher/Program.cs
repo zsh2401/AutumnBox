340db81551e406f880af25e329b1d195d5df1677
zsh2401 <zsh2401@163.com>
2018-09-16 23:50:54 +0800

    不再生成不该生成的调试信息

--- AutumnBox.GUI.Launcher/Program.cs
+++ AutumnBox.GUI.Launcher/Program.cs
@@ -11,6 +11,7 @@ namespace AutumnBox.GUI.Launcher
     {
         static void Main(string[] args)
         {
+            System.Text.Encoding.GetEncoding("GBK");
             new Program().Run(args);
         }
 
