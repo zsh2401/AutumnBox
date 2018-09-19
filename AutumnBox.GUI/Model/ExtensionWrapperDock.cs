f2de8bf6af92e392fd6049d26a8a705f169b6027
zsh2401 <zsh2401@163.com>
2018-09-19 14:10:35 +0800

    尝试优化，但效果并不明显，蛋疼啊

---
+++ AutumnBox.GUI/Model/ExtensionWrapperDock.cs
@@ -0,0 +1,78 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/9/19 11:52:25 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.GUI.MVVM;
+using AutumnBox.OpenFramework.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AutumnBox.GUI.Model
+{
+    internal class ExtensionWrapperDock : ModelBase
+    {
+        public string ToolTip
+        {
+            get
+            {
+                return Wrapper.Info.Name + System.Environment.NewLine +
+                     Wrapper.Info.FormatedDesc;
+            }
+        }
+        public IExtensionWrapper Wrapper { get; private set; }
+        public string Name => Wrapper.Info.Name;
+        public IExtInfoGetter Info => Wrapper.Info;
+        public ImageSource Icon
+        {
+            get
+            {
+                if (icon == null)
+                {
+                    LoadIcon();
+                }
+                return icon;
+            }
+        }
+        private ImageSource icon;
+        public ExtensionWrapperDock(IExtensionWrapper wrapper)
+        {
+            this.Wrapper = wrapper;
+            LoadIcon();
+        }
+        private void LoadIcon()
+        {
+            if (Wrapper.Info.Icon == null)
+            {
+                icon = App.Current.Resources["DefaultExtensionIcon"] as ImageSource;
+            }
+            else
+            {
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = new MemoryStream(Info.Icon);
+                bmp.EndInit();
+                bmp.Freeze();
+                icon = bmp;
+            }
+        }
+    }
+    internal static class ExtensionWrapperDockExtensions
+    {
+        public static IEnumerable<ExtensionWrapperDock> ToDocks(this IEnumerable<IExtensionWrapper> wrappers)
+        {
+            List<ExtensionWrapperDock> result = new List<ExtensionWrapperDock>();
+            foreach (var wrapper in wrappers)
+            {
+                result.Add(new ExtensionWrapperDock(wrapper));
+            }
+            return result;
+        }
+    }
+}
