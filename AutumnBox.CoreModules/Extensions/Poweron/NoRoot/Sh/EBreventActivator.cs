2c8d2a5fb11d712b5ceb4203d3902b328ba2dae4
zsh2401 <zsh2401@163.com>
2018-09-20 21:05:41 +0800

    完善黑域激活器

--- AutumnBox.CoreModules/Extensions/Poweron/NoRoot/Sh/EBreventActivator.cs
+++ AutumnBox.CoreModules/Extensions/Poweron/NoRoot/Sh/EBreventActivator.cs
@@ -3,10 +3,13 @@
 ** date:  2018/8/13 9:01:58 (UTC +8:00)
 ** desc： ...
 *************************************************/
+using AutumnBox.Basic.Calling;
+using AutumnBox.Basic.Calling.Adb;
 using AutumnBox.Basic.Device;
 using AutumnBox.Basic.Device.Management.AppFx;
 using AutumnBox.Basic.Util;
 using AutumnBox.OpenFramework.Extension;
+using System.Threading;
 
 namespace AutumnBox.CoreModules.Extensions.Poweron.NoRoot.Sh
 {
@@ -18,13 +21,31 @@ namespace AutumnBox.CoreModules.Extensions.Poweron.NoRoot.Sh
     [ExtIcon("Icons.brevent.png")]
     internal class EBreventActivator : OfficialVisualExtension
     {
+        private const string SH_PATH = "/data/data/me.piebridge.brevent/brevent.sh";
+        private const int stateCheck = 0;
+        private const int stateExecutingShell = 1;
+        private bool requiredStop = false;
+        private int state = 0;
+        private IProcessBasedCommand executingCommand;
         protected override int VisualMain()
         {
+            WriteInitInfo();
             new ActivityManager(TargetDevice).StartActivity("me.piebridge.brevent", "ui.BreventActivity");
-            WriteRunning();
-            var result = TargetDevice.GetShellCommand($"sh /data/data/me.piebridge.brevent/brevent.sh")
-                .To(OutputPrinter)
-                .Execute();
+            var catCommand = new ShellCommand(TargetDevice, $"cat {SH_PATH}");
+            state = stateCheck;
+            while (catCommand.Execute().ExitCode != (int)LinuxReturnCode.None && !requiredStop)
+            {
+                Ux.ShowMessageDialog(Res("EBreventActivatorFirstMsg"));
+                Thread.Sleep(2000);
+            }
+            if (requiredStop)
+            {
+                return ERR_CANCLLED_BY_USER;
+            }
+            state = stateExecutingShell;
+            executingCommand = TargetDevice.GetShellCommand($"sh {SH_PATH}")
+                .To(OutputPrinter);
+            var result = executingCommand.Execute();
             WriteExitCode(result.ExitCode);
             if (result.ExitCode == (int)LinuxReturnCode.None)
             {
@@ -33,6 +54,19 @@ namespace AutumnBox.CoreModules.Extensions.Poweron.NoRoot.Sh
             else
             {
                 return ERR;
+            }
+        }
+        protected override bool VisualStop()
+        {
+            if (state == stateCheck)
+            {
+                requiredStop = true;
+                return true;
+            }
+            else
+            {
+                executingCommand.Kill();
+                return true;
             }
         }
     }
