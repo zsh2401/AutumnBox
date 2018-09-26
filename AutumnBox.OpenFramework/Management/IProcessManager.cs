7adb4f8db34ef99dffa34888fa66715b032eddcf
zsh2401 <zsh2401@163.com>
2018-09-26 16:40:34 +0800

    AutumnBox.GUI 1.1.8 AutumnBox.OpenFx 8.4 AutumnBox.Basic 18.9.26

---
+++ AutumnBox.OpenFramework/Management/IProcessManager.cs
@@ -0,0 +1,14 @@
+﻿using AutumnBox.OpenFramework.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.OpenFramework.Management
+{
+    interface IProcessManager
+    {
+        int AllocatePid();
+        void Add(IExtensionProcess proc);
+        void Remove(IExtensionProcess proc);
+    }
+}
