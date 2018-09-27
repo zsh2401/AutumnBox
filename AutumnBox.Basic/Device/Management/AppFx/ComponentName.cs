b48b11e8c81b80ad96adf28b2c58475853eb5e48
zsh2401 <zsh2401@163.com>
2018-09-27 11:11:45 +0800

    完善注释，标准化命令创建方式

--- AutumnBox.Basic/Device/Management/AppFx/ComponentName.cs
+++ AutumnBox.Basic/Device/Management/AppFx/ComponentName.cs
@@ -4,14 +4,32 @@ using System.Text;
 
 namespace AutumnBox.Basic.Device.Management.AppFx
 {
+    /// <summary>
+    /// 组件名
+    /// </summary>
     public class ComponentName
     {
+        /// <summary>
+        /// 包名
+        /// </summary>
         public string PackageName { get; private set; }
+        /// <summary>
+        /// 类名
+        /// </summary>
         public string ClassName { get; private set; }
+        /// <summary>
+        /// 通过包名和类名构造此类
+        /// </summary>
+        /// <param name="pkgName"></param>
+        /// <param name="className"></param>
         public ComponentName(string pkgName,string className) {
             this.PackageName = pkgName;
             this.ClassName = className;
         }
+        /// <summary>
+        /// 字符串化
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{PackageName}/.{ClassName}";
