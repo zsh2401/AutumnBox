2b5dbef8f19c31f370905064922c886564d3ad8d
zsh2401 <zsh2401@163.com>
2018-09-28 04:13:04 +0800

    重新实现DeviceBuildPropSetter

---
+++ AutumnBox.Basic/Device/Management/OS/DeviceBuildPropManager.cs
@@ -0,0 +1,55 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/9/28 4:02:30 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.Basic.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Device.Management.OS
+{
+    public class DeviceBuildPropManager : DeviceCommander
+    {
+        public DeviceBuildPropManager(IDevice device) : base(device)
+        {
+        }
+        public virtual string this[string key]
+        {
+            get
+            {
+                return Get(key);
+            }
+            set
+            {
+                Set(key, value);
+            }
+        }
+        public virtual string Get(string key)
+        {
+            var result = CmdStation.GetShellCommand(Device,
+                $"getprop {key}")
+                .To(RaiseOutput)
+                .Execute();
+            return result.ExitCode == 0 ? result.Output.ToString() : null;
+        }
+        public virtual void Set(string key, string value)
+        {
+            SettingCheck();
+            CmdStation.GetShellCommand(Device,
+                $"setprop {key} {value}")
+                .To(RaiseOutput)
+                .Execute()
+                .ThrowIfShellExitCodeNotEqualsZero();
+        }
+        protected virtual void SettingCheck()
+        {
+            if (!Device.HaveSU())
+            {
+                throw new Exceptions.DeviceHasNoSuException();
+            }
+        }
+        protected virtual void GettingCheck() { }
+    }
+}
--- AutumnBox.Basic/Device/Management/OS/DeviceBuildPropSetter.cs
+++ AutumnBox.Basic/Device/Management/OS/DeviceBuildPropSetter.cs
@@ -11,98 +11,31 @@ namespace AutumnBox.Basic.Device.Management.OS
 {
     [Obsolete("由于安卓碎片化原因,此类不保证可以正常运行,请自行实现相关功能", true)]
 #pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
-    public class DeviceBuildPropSetter /*: IDisposable*/
+    public class DeviceBuildPropSetter : DeviceCommander
     {
-        //        private static readonly CommandExecuter executer = new CommandExecuter();
-        //        private AndroidShell shellAsSu;
-        //        public DeviceSerialNumber DeviceSerial { get; private set; }
-        //        public bool AutoSave { get; set; } = true;
-        //        public string CurrentString { get; private set; }
-        //        public AndroidShell ShellAsSu
-        //        {
-        //            private get
-        //            {
-        //                if (shellAsSu == null)
-        //                {
-        //                    shellAsSu = new AndroidShell(DeviceSerial);
-        //                }
-        //                return shellAsSu;
-        //            }
-        //            set
-        //            {
-        //                if (shellAsSu == null)
-        //                {
-        //                    shellAsSu = value;
-        //                }
-        //            }
-        //        }
+        public DeviceBuildPropSetter(IDevice device) : base(device)
+        {
+            if (!device.HaveSU())
+            {
+                throw new Exceptions.DeviceHasNoSuException();
+            }
+            Manager = new NoSuCheckManager(device);
+        }
 
-        //        private const string buildPropPath = "/system/build.prop";
-        //        private const string buildPropFileNameOnTempFloder = "now_build.prop.tmp";
-        //        private const string buildPropFileNameOnDeviceTemp = "/sdcard/atmb_buildprop_temp";
-        //        public DeviceBuildPropSetter(DeviceSerialNumber serial)
-        //        {
-        //            this.DeviceSerial = serial;
-        //            shellAsSu = new AndroidShell(serial);
-        //            shellAsSu.Connect();
-        //            if (!shellAsSu.Switch2Su())
-        //            {
-        //                throw new DeviceHasNoSuException();
-        //            }
-        //            ReloadFromDevice();
-        //        }
-        //        /// <summary>
-        //        /// 从设备加载数据
-        //        /// </summary>
-        //        public void ReloadFromDevice()
-        //        {
-        //            CurrentString = shellAsSu.SafetyInput($"cat {buildPropPath}").All.ToString();
-        //        }
-        //        /// <summary>
-        //        /// 将所做的改动保存到设备
-        //        /// </summary>
-        //        public void SaveToDevice()
-        //        {
-        //            //using (FileStream fs = TemporaryFilesHelper.GetTempFileStream(buildPropFileNameOnTempFloder))
-        //            //{
-        //            //    using (TextWriter writer = new StreamWriter(fs))
-        //            //    {
-        //            //        writer.Write(CurrentString);
-        //            //    }
-        //            //}
-        //            ////push local temp file to device sdcard
-        //            //var result= executer.Execute(Command.MakeForAdb(DeviceSerial, $"push {TemporaryFilesHelper.TemporaryFloderName}/{buildPropFileNameOnTempFloder} {buildPropFileNameOnDeviceTemp}"));
-        //            ////remount device /system rw
-        //            //shellAsSu.SafetyInput($"mount -o rw,remount /system");
-        //            ////move temp file on device to build.prop
-        //            //ShellAsSu.SafetyInput($"mv {buildPropFileNameOnDeviceTemp} {buildPropPath}");
-        //        }
-        //        public void Dispose()
-        //        {
-        //            shellAsSu.Dispose();
-        //        }
-        //        /// <summary>
-        //        /// 设置Value
-        //        /// </summary>
-        //        /// <param name="key"></param>
-        //        /// <param name="value"></param>
-        //        public void Set(string key, string value)
-        //        {
-        //            if (HaveThisProperty(key)) {
-        //                CurrentString = Regex.Replace(CurrentString, $"^{key}=.+$", $"{key}={value}", RegexOptions.Multiline);
-        //            }
-        //            {
-        //                CurrentString += $"\n{key}={value}";
-        //            }
-        //            if (AutoSave)
-        //            {
-        //                SaveToDevice();
-        //            }
-        //        }
-
-        //        public bool HaveThisProperty(string key) {
-        //            return Regex.IsMatch(CurrentString,$"^{key}=.+$",RegexOptions.Multiline);
-        //        }
+        private class NoSuCheckManager : DeviceBuildPropManager
+        {
+            public NoSuCheckManager(IDevice device) : base(device)
+            {
+            }
+            protected override void SettingCheck()
+            {
+            }
+        }
+        private DeviceBuildPropManager Manager { get; set; }
+        public void Set(string key, string value)
+        {
+            Manager.CmdStation = this.CmdStation;
+            Manager.Set(key, value);
+        }
     }
-    //#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
 }
