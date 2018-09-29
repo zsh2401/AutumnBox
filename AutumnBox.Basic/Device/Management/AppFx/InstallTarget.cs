181dbf0b16f82f28b43fc24cbb84de86a171fe20
zsh2401 <zsh2401@163.com>
2018-09-29 23:52:38 +0800

    AutumnBoxExtension新工具函数Step(),PackageManager挖了更多坑

---
+++ AutumnBox.Basic/Device/Management/AppFx/InstallTarget.cs
@@ -0,0 +1,31 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/9/29 23:50:54 (UTC +8:00)
+** desc： ...
+*************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Device.Management.AppFx
+{
+    /// <summary>
+    /// 决定包安装的位置
+    /// </summary>
+    public enum InstallTarget
+    {
+        /// <summary>
+        /// Default
+        /// </summary>
+        Default,
+        /// <summary>
+        /// Install package on the internal system memory.
+        /// </summary>
+        InternalSystemMemory,
+        /// <summary>
+        /// Install package on the shared mass storage (such as sdcard).
+        /// </summary>
+        [Obsolete("Nowadays,install apps to sdcard will let your device slowly")]
+        SharedMassStorage
+    }
+}
---
+++ AutumnBox.Basic/Device/Management/AppFx/PackageManagerInstallOption.cs
@@ -0,0 +1,43 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/9/29 23:26:37 (UTC +8:00)
+** desc： ...
+*************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Device.Management.AppFx
+{
+    /// <summary>
+    /// 包安装的参数
+    /// </summary>
+    public enum PackageManagerInstallOption
+    {
+        /// <summary>
+        ///  Install the package with forward lock.
+        /// </summary>
+        Lock,
+        /// <summary>
+        /// Reinstall an existing app, keeping its data.
+        /// </summary>
+        Reinstall,
+        /// <summary>
+        /// Allow test APKs to be installed
+        /// </summary>
+        Test,
+        /// <summary>
+        /// Install package on the shared mass storage (such as sdcard).
+        /// </summary>
+        Shared,
+        /// <summary>
+        /// Allow version code downgrade.
+        /// </summary>
+        AllowVersionDownGrade,
+        /// <summary>
+        /// Grant all permissions listed in the app manifest.
+        /// </summary>
+        GrantAllPermissions,
+    }
+
+}
