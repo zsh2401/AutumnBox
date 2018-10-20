e97bb4cfdce5e1fe830a16f703cd552873309066
zsh2401 <zsh2401@163.com>
2018-10-20 21:17:20 +0800

    修复一些小问题，新的特性机制基本完成

---
+++ AutumnBox.OpenFramework/Wrapper/ClassExtensionScanner.cs
@@ -0,0 +1,82 @@
+﻿using AutumnBox.OpenFramework.Extension;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AutumnBox.OpenFramework.Wrapper
+{
+    /// <summary>
+    /// ClassExtension scanner
+    /// </summary>
+    public sealed class ClassExtensionScanner
+    {
+        /// <summary>
+        /// 扫描设置
+        /// </summary>
+        public enum ScanOption
+        {
+            /// <summary>
+            /// 扫描信息
+            /// </summary>
+            Informations = 1,
+            /// <summary>
+            /// 扫描切面
+            /// </summary>
+            BeforeCreatingAspect = 1 << 1
+        }
+        private readonly Type type;
+        /// <summary>
+        /// 创造
+        /// </summary>
+        /// <param name="type"></param>
+        public ClassExtensionScanner(Type type)
+        {
+            this.type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+        /// <summary>
+        /// 执行扫描操作
+        /// </summary>
+        /// <param name="options"></param>
+        public void Scan(ScanOption options)
+        {
+            if (options.HasFlag(ScanOption.Informations))
+            {
+                ScanInformations();
+            }
+            if (options.HasFlag(ScanOption.BeforeCreatingAspect))
+            {
+                ScanAspects();
+                Debug.WriteLine($"have {BeforeCreatingAspects.Count()} aspects");
+            }
+        }
+        private void ScanAspects()
+        {
+            Type interfaceType = typeof(IBeforeCreatingAspect);
+            BeforeCreatingAspects = (from attr in type.GetCustomAttributes(true)
+                                     where interfaceType.IsAssignableFrom(attr.GetType())
+                                     select (IBeforeCreatingAspect)attr).ToArray();
+        }
+        private void ScanInformations()
+        {
+            Type interfaceType = typeof(IInformationAttribute);
+            Informations = new Dictionary<string, IInformationAttribute>();
+            var informatons = from attr in type.GetCustomAttributes(true)
+                              where interfaceType.IsAssignableFrom(attr.GetType())
+                              select (IInformationAttribute)attr;
+            foreach (var info in informatons)
+            {
+                Informations.Add(info.Key, info);
+            }
+        }
+        /// <summary>
+        /// 被扫描出来的信息
+        /// </summary>
+        public Dictionary<string, IInformationAttribute> Informations { get; private set; }
+        /// <summary>
+        /// 被扫描出来的切面
+        /// </summary>
+        public IBeforeCreatingAspect[] BeforeCreatingAspects { get; private set; }
+    }
+}
