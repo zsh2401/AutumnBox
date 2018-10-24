327a920a824eac253f6f75ddf6bd1d877567c491
zsh2401 <zsh2401@163.com>
2018-10-24 20:31:29 +0800

    重新实现UserManager

--- AutumnBox.Basic/Device/Management/OS/UserManager.cs
+++ AutumnBox.Basic/Device/Management/OS/UserManager.cs
@@ -3,6 +3,7 @@
 ** date:  2018/2/21 23:44:34 (UTC +8:00)
 ** desc： ...
 *************************************************/
+using AutumnBox.Basic.Data;
 using AutumnBox.Basic.Util;
 using System;
 using System.Collections.Generic;
@@ -10,53 +11,31 @@ using System.Text.RegularExpressions;
 
 namespace AutumnBox.Basic.Device.Management.OS
 {
-    /// <summary>
-    /// 用户信息
-    /// </summary>
-    public class User
-    {
-        /// <summary>
-        /// UID
-        /// </summary>
-        public int Id { get; set; }
-        /// <summary>
-        /// 用户名
-        /// </summary>
-        public string Name { get; set; }
-    }
+
     /// <summary>
     /// 用户管理器
     /// </summary>
-    [Obsolete("等待重置")]
-    public class UserManager
+    public class UserManager : DeviceCommander, IReceiveOutputByTo<UserManager>
     {
+        private const string PATTERN_USER_INFO = @"UserInfo{(?<id>\d+):(?<name>.+):";
         private static readonly Regex userInfoRegex;
-        private readonly IDevice device;
         static UserManager()
         {
-            userInfoRegex = new Regex(@"UserInfo{(?<id>\d+):(?<name>.+):", RegexOptions.Multiline);
+            userInfoRegex = new Regex(PATTERN_USER_INFO, RegexOptions.Multiline);
         }
         /// <summary>
-        /// 构造一个用户管理器
+        /// 从输出中获取用户数据
         /// </summary>
-        /// <param name="device"></param>
-        public UserManager(IDevice device)
+        /// <param name="output"></param>
+        /// <param name="ignoreZeroUser"></param>
+        /// <returns></returns>
+        protected virtual UserInfo[] ParseOutput(Output output, bool ignoreZeroUser)
         {
-            this.device = device;
-        }
-        /// <summary>
-        /// 获取设备上的所有用户
-        /// </summary>
-        /// <param name="ignoreZeroUser">是否忽略0号用户</param>
-        /// <returns>用户</returns>
-        public User[] GetUsers(bool ignoreZeroUser =true)
-        {
-            var output = device.Shell("pm list users");
             var matches = userInfoRegex.Matches(output.ToString());
-            List<User> users = new List<User>();
+            List<UserInfo> users = new List<UserInfo>();
             foreach (Match match in matches)
             {
-                var user = new User()
+                var user = new UserInfo()
                 {
                     Id = int.Parse(match.Result("${id}")),
                     Name = match.Result("${name}")
@@ -67,13 +46,48 @@ namespace AutumnBox.Basic.Device.Management.OS
             return users.ToArray();
         }
         /// <summary>
+        /// 构造一个用户管理器
+        /// </summary>
+        /// <param name="device"></param>
+        public UserManager(IDevice device) : base(device)
+        {
+        }
+        /// <summary>
+        /// 获取设备上的所有用户
+        /// </summary>
+        /// <param name="ignoreZeroUser">是否忽略0号用户</param>
+        /// <returns>用户</returns>
+        public UserInfo[] GetUsers(bool ignoreZeroUser = true)
+        {
+            var executeResult = CmdStation
+                .GetShellCommand(Device, "pm list users")
+                .To(RaiseOutput)
+                .Execute()
+                .ThrowIfShellExitCodeNotEqualsZero();
+            return ParseOutput(executeResult.Output, ignoreZeroUser);
+        }
+        /// <summary>
         /// 移除某个用户
         /// </summary>
         /// <param name="uid">UID</param>
         /// <returns></returns>
         public void RemoveUser(int uid)
         {
-            device.Shell($"pm remove-user {uid}").ThrowIfExitCodeNotEqualsZero();
+            CmdStation
+               .GetShellCommand(Device, $"pm remove-user {uid}")
+               .To(RaiseOutput)
+               .Execute()
+               .ThrowIfShellExitCodeNotEqualsZero();
+        }
+        /// <summary>
+        /// To模式订阅输出内容
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public UserManager To(Action<OutputReceivedEventArgs> callback)
+        {
+            RegisterToCallback(callback);
+            return this;
         }
     }
 }
