f6a1722ce8e8c685f6da66dba5ad70771e72387c
zsh2401 <zsh2401@163.com>
2018-10-25 10:35:21 +0800

    加入与实现暴力清空器,但未测试

---
+++ AutumnBox.CoreModules/Extensions/Poweron/NoRoot/EClearAccounts.cs
@@ -0,0 +1,40 @@
+﻿using AutumnBox.CoreModules.Aspect;
+using AutumnBox.CoreModules.Lib;
+using AutumnBox.OpenFramework.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.CoreModules.Extensions.Poweron.NoRoot
+{
+    [ExtName("Clear all accounts", "zh-CN:暴力清空所有账号")]
+    [ExtDesc("Use the tech by web1n", "zh-CN:使用web1n提供的黑科技暴力清空账号")]
+    [UserAgree("EClearAccountsWarning")]
+    [ExtIcon("Icons.nuclear.png")]
+    [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
+    class EClearAccounts : OfficialVisualExtension
+    {
+        protected override int VisualMain()
+        {
+            var gp = new GodPower(this, TargetDevice)
+            {
+                CmdStation = this.CmdStation
+            };
+            Progress = 20;
+            gp.Extract();
+            Progress = 50;
+            gp.GetPushCommand()
+                .To(OutputPrinter)
+                .Execute();
+            Progress = 80;
+            var result = gp.GetRemoveAccountCommnad()
+                 .To(OutputPrinter)
+                 .Execute();
+            Progress = 100;
+            WriteExitCode(result.ExitCode);
+            return result.ExitCode;
+        }
+    }
+}
