a8e5df21c362821456f93b63e8b7827545f68a39
zsh2401 <zsh2401@163.com>
2018-11-09 11:41:38 +0800

    简单实现RealtimeShellCommand

---
+++ AutumnBox.Basic/Calling/Adb/RealtimeShellCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AutumnBox.Basic.Calling.Adb
+{
+    class RealtimeShellCommand : AdbCommand
+    {
+        private readonly string shellCommand;
+        public RealtimeShellCommand(string shellCommand) : base("shell")
+        {
+            this.shellCommand = shellCommand ?? throw new ArgumentNullException(nameof(shellCommand));
+        }
+        protected override void BeforeProcessStart(ProcessStartInfo procStartInfo)
+        {
+            base.BeforeProcessStart(procStartInfo);
+            procStartInfo.RedirectStandardInput = true;
+        }
+        protected override void OnProcessStarted(Process proc)
+        {
+            base.OnProcessStarted(proc);
+            proc.StandardInput.WriteLine(shellCommand);
+        }
+    }
+}
