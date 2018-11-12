23c526d4a65848d7528d3eb36603899d9d57d898
zsh2401 <zsh2401@163.com>
2018-11-12 13:46:10 +0800

    基于web1n提供的新DPM操作器完成

---
+++ AutumnBox.Basic/Exceptions/CommandFailedException.cs
@@ -0,0 +1,41 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/11/12 12:28:26 (UTC +8:00)
+** desc： ...
+*************************************************/
+using AutumnBox.Basic.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Exceptions
+{
+    /// <summary>
+    /// 命令失败异常
+    /// </summary>
+    public class CommandFailedException : Exception
+    {
+        /// <summary>
+        /// 返回码
+        /// </summary>
+        public int? ExitCode { get; } = null;
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="exitCode"></param>
+        public CommandFailedException(string output, int? exitCode=null) : base(output)
+        {
+            ExitCode = exitCode;
+        }
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="exitCode"></param>
+        public CommandFailedException(Output output, int? exitCode=null)
+            : this(output.ToString(), exitCode)
+        {
+        }
+    }
+}
--- AutumnBox.CoreModules/Extensions/Poweron/EStopApp.cs
+++ AutumnBox.CoreModules/Extensions/Poweron/EStopApp.cs
@@ -15,18 +15,11 @@ namespace AutumnBox.CoreModules.Extensions.Poweron
     [ExtAppProperty(PKG_NAME)]
     [ExtIcon("Icons.stopapp.png")]
     [ExtPriority(ExtPriority.HIGH)]
-    [DpmReceiver("待会儿覆写方法，反正也用不到这里，呵呵哒，瞎写！")]
+    [DpmReceiver(COMPONENT_NAME)]
     [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
     internal class EStopApp : EDpmSetterBase
     {
         private const string PKG_NAME = "web1n.stopapp";
-        protected override int SetDpm()
-        {
-            return CmdStation.Register(
-                GodPower
-                .GetSetStopAppCommand())
-                .To(OutputPrinter)
-                .Execute().ExitCode;
-        }
+        private const string COMPONENT_NAME = "web1n.stopapp/.receiver.AdminReceiver";
     }
 }
