db696b9e1d84b56a41ee3c3d252f661bb02297e9
zsh2401 <zsh2401@163.com>
2018-11-16 09:53:57 +0800

    AutumnBox.Basic拓展API新设计

---
+++ AutumnBox.Basic/Extension/ExtensionShared.cs
@@ -0,0 +1,20 @@
+﻿using AutumnBox.Basic.Calling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Extension
+{
+    internal static class ExtensionShared
+    {
+        public readonly static CommandStation CommandStation;
+        static ExtensionShared()
+        {
+            CommandStation = new CommandStation();
+        }
+        public static CommandStation NullCheckAndGet(this CommandStation cmdStation)
+        {
+            return cmdStation ?? CommandStation;
+        }
+    }
+}
--- AutumnBox.Basic/Extension/FileInfoExtension.cs
+++ AutumnBox.Basic/Extension/FileInfoExtension.cs
@@ -3,6 +3,7 @@
 ** date:  2018/8/29 0:15:54 (UTC +8:00)
 ** desc： ...
 *************************************************/
+using AutumnBox.Basic.Calling;
 using AutumnBox.Basic.Calling.Adb;
 using AutumnBox.Basic.Device;
 using AutumnBox.Basic.Exceptions;
@@ -22,7 +23,8 @@ namespace AutumnBox.Basic.Extension
         /// <param name="fileInfo"></param>
         /// <param name="device"></param>
         /// <param name="path"></param>
-        public static void PushTo(this FileInfo fileInfo, IDevice device, string path)
+        /// <param name="cmdStation"></param>
+        public static void PushTo(this FileInfo fileInfo, IDevice device, string path, CommandStation cmdStation = null)
         {
             if (fileInfo == null)
             {
@@ -38,7 +40,8 @@ namespace AutumnBox.Basic.Extension
             {
                 throw new ArgumentNullException(nameof(path));
             }
-            using (var command = new AdbCommand(device, $"push \"{fileInfo.FullName}\" \"{path}\""))
+            using (var command = cmdStation.NullCheckAndGet()
+                .GetAdbCommand(device, $"push \"{fileInfo.FullName}\" \"{path}\""))
             {
                 var result = command.Execute();
                 if (result.ExitCode != 0)
@@ -52,7 +55,8 @@ namespace AutumnBox.Basic.Extension
         /// </summary>
         /// <param name="apkFileInfo"></param>
         /// <param name="device"></param>
-        public static void InstallTo(this FileInfo apkFileInfo, IDevice device)
+        /// <param name="cmdStation"></param>
+        public static void InstallTo(this FileInfo apkFileInfo, IDevice device, CommandStation cmdStation = null)
         {
             if (apkFileInfo == null)
             {
@@ -63,12 +67,12 @@ namespace AutumnBox.Basic.Extension
             {
                 throw new ArgumentNullException(nameof(device));
             }
-
             if (apkFileInfo.Extension != ".apk")
             {
                 throw new ArgumentException("Is not apk file!", nameof(apkFileInfo));
             }
-            using (var command = new AdbCommand(device, $"install \"{apkFileInfo.FullName}\""))
+            using (var command = cmdStation.NullCheckAndGet()
+                .GetAdbCommand(device, $"install \"{apkFileInfo.FullName}\""))
             {
                 var result = command.Execute();
                 if (result.ExitCode != 0)
