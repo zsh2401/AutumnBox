021e9685e69cce60e23202241ea24e005a5bcbab
zsh2401 <zsh2401@163.com>
2018-11-30 16:04:17 +0800

    Reporter半成品

---
+++ AutumnBox.Reporter/MVVM/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿/*************************************************
+** auth： zsh2401@163.com
+** date:  2018/8/15 16:21:56 (UTC +8:00)
+** desc： ...
+*************************************************/
+
+using System.Runtime.CompilerServices;
+
+namespace AutumnBox.Reporter.MVVM
+{
+    class ViewModelBase : NotificationObject
+    {
+        protected virtual bool RaisePropertyChangedOnDispatcher { get; set; } = false;
+        protected override void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (RaisePropertyChangedOnDispatcher)
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    base.RaisePropertyChanged(propertyName);
+                });
+            }
+            else
+            {
+                base.RaisePropertyChanged(propertyName);
+            }
+
+        }
+    }
+}
---
+++ AutumnBox.Reporter/ViewModel/VMReporter.cs
@@ -0,0 +1,95 @@
+﻿using AutumnBox.Reporter.Model;
+using AutumnBox.Reporter.MVVM;
+using AutumnBox.Reporter.Util;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AutumnBox.Reporter.ViewModel
+{
+    class VMReporter : ViewModelBase
+    {
+        public double UploadProgress
+        {
+            get => _uploadProgress; set
+            {
+                _uploadProgress = value;
+                RaisePropertyChanged();
+            }
+        }
+        private double _uploadProgress;
+
+        public Visibility UploadingPanelVisibily
+        {
+            get => _uploadingPanelVisibily; set
+            {
+                _uploadingPanelVisibily = value;
+                RaisePropertyChanged();
+            }
+        }
+        private Visibility _uploadingPanelVisibily = Visibility.Hidden;
+
+        public ReportHeader Header
+        {
+            get => _header; set
+            {
+                _header = value;
+                RaisePropertyChanged();
+            }
+        }
+        private ReportHeader _header;
+
+        public IEnumerable<Log> Logs
+        {
+            get => _logs;
+            set
+            {
+                _logs = value;
+                RaisePropertyChanged();
+            }
+        }
+        private IEnumerable<Log> _logs;
+
+        public ICommand Submit
+        {
+            get => _submit; set
+            {
+                _submit = value;
+                RaisePropertyChanged();
+            }
+        }
+        private ICommand _submit;
+
+        public VMReporter()
+        {
+            Header = new ReportHeader()
+            {
+                UUID = Guid.NewGuid().ToString(),
+            };
+            Submit = new FlexiableCommand(() =>
+            {
+                UploadingPanelVisibily = Visibility.Visible;
+                UploadProgress = 0;
+                var uploader = new Uploader(Header);
+                int uploaded = 0;
+                uploader.Upload(Logs, (ex) =>
+                {
+                    if (ex != null)
+                    {
+                        Trace.WriteLine(ex);
+                    }
+                    UploadProgress = uploaded / Logs.Count() * 100;
+                    if (uploaded == Logs.Count())
+                    {
+                        UploadingPanelVisibily = Visibility.Hidden;
+                    }
+                });
+            });
+        }
+    }
+}
