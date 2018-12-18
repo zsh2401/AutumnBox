28b03bd3705456d3684fcaae5c9fa25268e0b32c
zsh2401 <zsh2401@163.com>
2018-12-18 17:39:33 +0800

    CommandExecutor拥有了更强的拓展性

--- AutumnBox.Basic/Calling/CommandExecutor.cs
+++ AutumnBox.Basic/Calling/CommandExecutor.cs
@@ -10,82 +10,156 @@ using System.Threading.Tasks;
 
 namespace AutumnBox.Basic.Calling
 {
-    public sealed class CommandExecutor : INotifyOutput, IDisposable, IReceiveOutputByTo<CommandExecutor>
+    /// <summary>
+    /// 命令执行器
+    /// </summary>
+    public class CommandExecutor : INotifyOutput, IDisposable, IReceiveOutputByTo<CommandExecutor>
     {
+        /// <summary>
+        /// 命令执行器的结果
+        /// </summary>
         public class Result
         {
-            public int ExitCode { get; }
-            public Output Output { get; }
+            /// <summary>
+            /// 返回码
+            /// </summary>
+            public virtual int ExitCode { get; }
+            /// <summary>
+            /// 输出内容
+            /// </summary>
+            public virtual Output Output { get; }
+            /// <summary>
+            /// 结果
+            /// </summary>
+            /// <param name="output"></param>
+            /// <param name="exitCode"></param>
             public Result(Output output, int exitCode)
             {
                 Output = output ?? throw new ArgumentNullException(nameof(output));
                 ExitCode = exitCode;
             }
+            /// <summary>
+            /// 结果
+            /// </summary>
+            protected Result() { }
         }
+        /// <summary>
+        /// 当接收到输出时发生
+        /// </summary>
         public event OutputReceivedEventHandler OutputReceived;
-
-        public void KillCurrent()
+        /// <summary>
+        /// 杀死当前正在执行的进程
+        /// </summary>
+        public virtual void KillCurrent()
         {
             currentProcess?.KillByTaskKill();
         }
-
-        public async Task<Result> CmdAsync(params string[] args)
+        /// <summary>
+        /// 异步执行CMD命令
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> CmdAsync(params string[] args)
         {
             return await Task.Run(() =>
             {
                 return Cmd(args);
             });
         }
-        public async Task<Result> AdbShellAsync(IDevice device, params string[] args)
+        /// <summary>
+        /// 异步执行ADB SHELL命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> AdbShellAsync(IDevice device, params string[] args)
         {
             return await Task.Run(() =>
             {
                 return AdbShell(device, args);
             });
         }
-        public async Task<Result> FastbootAsync(IDevice device, params string[] args)
+        /// <summary>
+        /// 异步执行针对Fastboot命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> FastbootAsync(IDevice device, params string[] args)
         {
             return await Task.Run(() =>
             {
                 return Fastboot(device, args);
             });
         }
-        public async Task<Result> AdbAsync(IDevice device, params string[] args)
+        /// <summary>
+        /// 异步执行针对设备的ADB命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> AdbAsync(IDevice device, params string[] args)
         {
             return await Task.Run(() =>
             {
                 return Adb(device, args);
             });
         }
-        public async Task<Result> FastbootAsync(params string[] args)
+        /// <summary>
+        /// 异步执行Fastboot命令
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> FastbootAsync(params string[] args)
         {
             return await Task.Run(() =>
             {
                 return Fastboot(args);
             });
         }
-        public async Task<Result> AdbAsync(params string[] args)
+        /// <summary>
+        /// 异步执行ADB命令
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual async Task<Result> AdbAsync(params string[] args)
         {
             return await Task.Run(() =>
              {
                  return Adb(args);
              });
         }
-
-        public Result Cmd(params string[] args)
+        /// <summary>
+        /// CMD
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Cmd(params string[] args)
         {
             string joined = string.Join(" ", args);
             string _args = $"/c {joined}";
             return Execute("cmd.exe", _args);
         }
-        public Result AdbShell(IDevice device, params string[] args)
+        /// <summary>
+        /// 执行adb shell命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result AdbShell(IDevice device, params string[] args)
         {
             IAdbServer adbServer = AutumnBox.Basic.ManagedAdb.Adb.Server;
             FileInfo exe = AutumnBox.Basic.ManagedAdb.Adb.FastbootFile;
             string joined = string.Join(" ", args);
             return Adb(device, $"shell {joined}");
         }
-        public Result Fastboot(IDevice device, params string[] args)
+        /// <summary>
+        /// 执行fastboot命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Fastboot(IDevice device, params string[] args)
         {
             IAdbServer adbServer = AutumnBox.Basic.ManagedAdb.Adb.Server;
             FileInfo exe = AutumnBox.Basic.ManagedAdb.Adb.FastbootFile;
@@ -93,7 +167,13 @@ namespace AutumnBox.Basic.Calling
             string compCommand = $"-s {device.SerialNumber} {joined}";
             return Execute(exe.FullName, compCommand);
         }
-        public Result Adb(IDevice device, params string[] args)
+        /// <summary>
+        /// 执行针对设备的adb命令
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Adb(IDevice device, params string[] args)
         {
             IAdbServer adbServer = AutumnBox.Basic.ManagedAdb.Adb.Server;
             FileInfo exe = AutumnBox.Basic.ManagedAdb.Adb.AdbFile;
@@ -101,7 +181,12 @@ namespace AutumnBox.Basic.Calling
             string compCommand = $"-P{adbServer.Port} -s {device.SerialNumber} {joined}";
             return Execute(exe.FullName, compCommand);
         }
-        public Result Fastboot(params string[] args)
+        /// <summary>
+        /// 执行非针对设备的fastboot命令
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Fastboot(params string[] args)
         {
             IAdbServer adbServer = AutumnBox.Basic.ManagedAdb.Adb.Server;
             FileInfo exe = AutumnBox.Basic.ManagedAdb.Adb.FastbootFile;
@@ -109,7 +194,12 @@ namespace AutumnBox.Basic.Calling
             string compCommand = $"{joined}";
             return Execute(exe.FullName, compCommand);
         }
-        public Result Adb(params string[] args)
+        /// <summary>
+        /// 执行非针对设备的adb命令
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Adb(params string[] args)
         {
             IAdbServer adbServer = AutumnBox.Basic.ManagedAdb.Adb.Server;
             FileInfo exe = AutumnBox.Basic.ManagedAdb.Adb.AdbFile;
@@ -117,63 +207,141 @@ namespace AutumnBox.Basic.Calling
             string compCommand = $"-P{adbServer.Port} {joined}";
             return Execute(exe.FullName, compCommand);
         }
-
-        public void Write(char _char)
+        /// <summary>
+        /// 向标准输入流输入一个字符
+        /// </summary>
+        /// <param name="_char"></param>
+        public virtual void Write(char _char)
         {
             currentProcess.StandardInput.Write(_char);
         }
-        public void Write(string str)
+        /// <summary>
+        /// 向标准输入流输入一串字符
+        /// </summary>
+        /// <param name="str"></param>
+        public virtual void Write(string str)
         {
             currentProcess.StandardInput.Write(str);
         }
-        public void WriteLine(string line)
+        /// <summary>
+        /// 想标准输入流输入一行字符
+        /// </summary>
+        /// <param name="line"></param>
+        public virtual void WriteLine(string line)
         {
             currentProcess.StandardInput.WriteLine(line);
         }
 
-        private Action<OutputReceivedEventArgs> callback;
-        private Process currentProcess;
-        private readonly OutputBuilder outputBuilder = new OutputBuilder();
-        private readonly object _lock = new object();
+        /// <summary>
+        /// TO订阅的callback
+        /// </summary>
+        protected Action<OutputReceivedEventArgs> callback;
+        /// <summary>
+        /// 当前进程
+        /// </summary>
+        protected Process currentProcess;
+        /// <summary>
+        /// 输出构造器
+        /// </summary>
+        protected readonly OutputBuilder outputBuilder = new OutputBuilder();
+        /// <summary>
+        /// 执行锁
+        /// </summary>
+        protected readonly object _lock = new object();
 
-        public Result Execute(string fileName, string args)
+        /// <summary>
+        /// 获取进程启动信息
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected virtual ProcessStartInfo GetStartInfo(string fileName, string args)
+        {
+            return new ProcessStartInfo()
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = false,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = fileName,
+                Arguments = args,
+            };
+        }
+        /// <summary>
+        /// 进程启动了,进行一些流处理等操作
+        /// </summary>
+        /// <param name="process"></param>
+        protected virtual void ProcessStarted(Process process)
+        {
+            process.OutputDataReceived += (s, e) => RaiseOutputReceived(e, false);
+            process.ErrorDataReceived += (s, e) => RaiseOutputReceived(e, true);
+            currentProcess.BeginOutputReadLine();
+            currentProcess.BeginErrorReadLine();
+        }
+        /// <summary>
+        /// 进程结束了,释放资源
+        /// </summary>
+        /// <param name="process"></param>
+        protected virtual void ProcessExited(Process process)
+        {
+            process.CancelErrorRead();
+            process.CancelOutputRead();
+            try { currentProcess?.Dispose(); } catch { }
+        }
+
+        /// <summary>
+        /// 无锁的执行..不建议覆写
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected virtual Result ExecuteWithoutLock(string fileName, string args)
+        {
+            if (disposedValue)
+            {
+                throw new ObjectDisposedException(nameof(CommandExecutor));
+            }
+            int exitCode;
+            ProcessStartInfo pStartInfo;
+            outputBuilder.Clear();
+            pStartInfo = GetStartInfo(fileName, args);
+            currentProcess = Process.Start(pStartInfo);
+            ProcessStarted(currentProcess);
+            currentProcess.WaitForExit();
+            exitCode = currentProcess.ExitCode;
+            ProcessExited(currentProcess);
+            currentProcess = null;
+            return new Result(outputBuilder.Result, exitCode);
+        }
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual Result Execute(string fileName, string args)
         {
             lock (_lock)
             {
-                if (disposedValue)
-                {
-                    throw new ObjectDisposedException(nameof(CommandExecutor));
-                }
-                outputBuilder.Clear();
-                var pStartInfo = new ProcessStartInfo()
-                {
-                    RedirectStandardError = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardInput = false,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    FileName = fileName,
-                    Arguments = args,
-                };
-                currentProcess = Process.Start(pStartInfo);
-                currentProcess.OutputDataReceived += (s, e) => RaiseOutputReceived(e, false);
-                currentProcess.ErrorDataReceived += (s, e) => RaiseOutputReceived(e, true);
-                currentProcess.BeginOutputReadLine();
-                currentProcess.BeginErrorReadLine();
-                currentProcess.WaitForExit();
-                currentProcess.CancelErrorRead();
-                currentProcess.CancelOutputRead();
-                int exitCode = currentProcess.ExitCode;
-                try { currentProcess?.Dispose(); currentProcess = null; } catch { }
-                return new Result(outputBuilder.Result, exitCode);
+                return ExecuteWithoutLock(fileName, args);
             }
         }
-        private void RaiseOutputReceived(DataReceivedEventArgs e, bool isErr)
+
+        /// <summary>
+        /// 触发输出事件
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="isErr"></param>
+        protected virtual void RaiseOutputReceived(DataReceivedEventArgs e, bool isErr)
         {
             callback?.Invoke(new OutputReceivedEventArgs(e, isErr));
             OutputReceived?.Invoke(this, new OutputReceivedEventArgs(e, isErr));
         }
 
+        /// <summary>
+        /// 构造CommandExecutor
+        /// </summary>
         public CommandExecutor()
         {
             outputBuilder.Register(this);
@@ -182,7 +350,11 @@ namespace AutumnBox.Basic.Calling
         #region IDisposable Support
         private bool disposedValue = false; // 要检测冗余调用
 
-        private void Dispose(bool disposing)
+        /// <summary>
+        /// protected Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
@@ -206,6 +378,9 @@ namespace AutumnBox.Basic.Calling
         // }
 
         // 添加此代码以正确实现可处置模式。
+        /// <summary>
+        /// Dispose
+        /// </summary>
         public void Dispose()
         {
             // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
@@ -214,7 +389,12 @@ namespace AutumnBox.Basic.Calling
             // GC.SuppressFinalize(this);
         }
 
-        public CommandExecutor To(Action<OutputReceivedEventArgs> callback)
+        /// <summary>
+        /// 通过TO模式进行输出的订阅
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public virtual CommandExecutor To(Action<OutputReceivedEventArgs> callback)
         {
             this.callback = callback ?? throw new ArgumentNullException(nameof(callback));
             return this;
