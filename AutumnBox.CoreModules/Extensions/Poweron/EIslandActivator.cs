fc516312b0fd19ddd1672d2e143d31d225906ff8
zsh2401 <zsh2401@163.com>
2018-12-29 15:37:15 +0800

    优化DPM SETTER,添加岛激活器

---
+++ AutumnBox.CoreModules/Extensions/Poweron/EIslandActivator.cs
@@ -0,0 +1,17 @@
+﻿using AutumnBox.CoreModules.Attribute;
+using AutumnBox.CoreModules.Lib;
+using AutumnBox.OpenFramework.Extension;
+
+namespace AutumnBox.CoreModules.Extensions.Poweron
+{
+    [ExtName("Activate Island by on key", "zh-cn:一键激活岛")]
+    [ExtAppProperty(PKG_NAME)]
+    [ExtIcon("Icons.Island.png")]
+    [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
+    [DpmReceiver(RECEIVER_NAME)]
+    class EIslandActivator : EDpmSetterBase
+    {
+        private const string PKG_NAME = "com.oasisfeng.island";
+        private const string RECEIVER_NAME = "com.oasisfeng.island/.IslandDeviceAdminReceiver";
+    }
+}
