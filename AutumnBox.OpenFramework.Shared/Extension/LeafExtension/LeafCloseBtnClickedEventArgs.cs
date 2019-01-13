1e3626bb2178e1496759f58b021e2f533155f4eb
zsh2401 <zsh2401@163.com>
2019-01-14 00:38:16 +0800

    完成LeafUI接口设计

---
+++ AutumnBox.OpenFramework.Shared/Extension/LeafExtension/LeafCloseBtnClickedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AutumnBox.OpenFramework.Extension.LeafExtension
+{
+    /// <summary>
+    /// LeafUI的关闭按钮被点击时发生的事件
+    /// </summary>
+    public class LeafCloseBtnClickedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 是否可被关闭
+        /// </summary>
+        public bool CanBeClosed { get; set; } = false;
+    }
+}
