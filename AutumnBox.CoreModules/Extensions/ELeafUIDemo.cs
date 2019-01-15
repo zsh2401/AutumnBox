353b95d9fd2dcd6bc8356908ce737ef2be2edbcc
zsh2401 <zsh2401@163.com>
2019-01-16 03:19:18 +0800

    删除报告器项目

--- AutumnBox.CoreModules/Extensions/ELeafUIDemo.cs
+++ AutumnBox.CoreModules/Extensions/ELeafUIDemo.cs
@@ -1,12 +1,5 @@
-﻿using AutumnBox.Basic.Device;
-using AutumnBox.OpenFramework.Extension;
+﻿using AutumnBox.OpenFramework.Extension;
 using AutumnBox.OpenFramework.Extension.LeafExtension;
-using AutumnBox.OpenFramework.Open;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutumnBox.CoreModules.Extensions
 {
