67270e6ace96ddb1cba672738930f6365ad286fd
zsh2401 <zsh2401@163.com>
2019-01-19 04:31:41 +0800

    暂未实现的DpmFailedMessageParser

---
+++ AutumnBox.CoreModules/Lib/DpmFailedMessageParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutumnBox.CoreModules.Lib
+{
+    internal static class DpmFailedMessageParser
+    {
+        public static bool TryParse(string output, out string tip, out string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
