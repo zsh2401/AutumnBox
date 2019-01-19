ea133abe6fa6509fd5acdcb9ace483eab255f8f7
zsh2401 <zsh2401@163.com>
2019-01-19 09:49:11 +0800

    重置小黑屋麦克斯韦激活器,AndroidShell对象现已加入

--- AutumnBox.CoreModules/Extensions/Poweron/EStopAppMaxWellActivator.cs
+++ AutumnBox.CoreModules/Extensions/Poweron/EStopAppMaxWellActivator.cs
@@ -1,37 +1,59 @@
 ﻿using AutumnBox.Basic.Calling;
+using AutumnBox.Basic.Device;
 using AutumnBox.Basic.Device.Management.AppFx;
+using AutumnBox.Basic.Device.ManagementV2;
 using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.Extension.LeafExtension;
+using System.Threading;
 
 namespace AutumnBox.CoreModules.Extensions.Poweron
 {
     [ExtName("激活小黑屋麦克斯韦妖模式", "en-us:StopApp MaxWell")]
+    [ExtAuth("zsh2401")]
     [ExtAppProperty(PKG_NAME)]
     [ExtPriority(ExtPriority.HIGH - 1)]
     [ExtIcon("Icons.stopapp.png")]
     [ExtRequiredDeviceStates(Basic.Device.DeviceState.Poweron)]
-    class EStopAppMaxWellActivator : OfficialVisualExtension
+    class EStopAppMaxWellActivator : LeafExtensionBase
     {
         private const string PKG_NAME = "web1n.stopapp";
         private const string MAIN_ACTIVITY = "web1n.stopapp/.activity.MainActivity";
         private const string ACTIVATE_BY_SH = "sh /sdcard/Android/data/web1n.stopapp/files/demon.sh";
         private const string ACTIVATE_COMMAND =
             "CLASSPATH=web1n.stopapp*/base.apk app_process /system/bin com.web1n.stopapp.app_process.DemonStart";
-
-        protected override int VisualMain()
+        [LMain]
+        private void Main(ILeafUI ui, IDevice device)
         {
-            //启动小黑屋确保sh被释放
-            var am = new ActivityManager(DeviceSelectedOnCreating) { CmdStation = this.CmdStation };
-            am.To(OutputPrinter);
-            try
+            //            # Script to start "web1n's demon" on the device, which has a very rudimentary
+            //# shell.
+            //#
+            //base=/system
+
+            //path=`pm path web1n.stopapp`
+            //path=${path:8}
+
+            //export CLASSPATH=$path
+            //exec app_process $base/bin com.web1n.stopapp.app_process.DemonStart "$@"
+            using (ui)
             {
-                am.StartActivity(MAIN_ACTIVITY);
-            }
-            catch { }
-            using (CommandExecutor executor = new CommandExecutor())
-            {
-                executor.To(OutputPrinter);
-                var result = executor.AdbShell(DeviceSelectedOnCreating, ACTIVATE_BY_SH);
-                return result.ExitCode;
+                var info = this.GetInformations();
+                ui.Icon = info.Icon;
+                ui.Title = info.Name;
+                ui.Show();
+                using (var shell = new AndroidShell(device))
+                {
+                    shell.To(e => ui.WriteOutput(e.Text));
+                    shell.Open();
+                    shell.WriteLine("base=/system");
+                    shell.WriteLine("path=`pm path web1n.stopapp`");
+                    shell.WriteLine("path=${path:8}");
+                    shell.WriteLine("export CLASSPATH=$path");
+                    shell.WriteLine("exec app_process $base/bin com.web1n.stopapp.app_process.DemonStart \"$@\"");
+                    shell.WriteLine("exit $?");
+                    shell.WaitForExit();
+                    ui.WriteOutput("exit code:" + shell.ExitCode);
+                    ui.Finish(shell.ExitCode == 0 ? AutumnBoxExtension.OK : AutumnBoxExtension.ERR);
+                }
             }
         }
     }
