0b07c40f181c3e64440749c0a91812c8bbace0de
zsh2401 <zsh2401@163.com>
2019-01-22 03:55:10 +0800

    增加ADB开源说明

--- AutumnBox.GUI/ViewModel/VMOpenSource.cs
+++ AutumnBox.GUI/ViewModel/VMOpenSource.cs
@@ -12,6 +12,7 @@ namespace AutumnBox.GUI.ViewModel
         private static readonly List<OpenSourceProject> projects =
             new List<OpenSourceProject>()
             {
+                  new OpenSourceProject("Android Debug Bridge","Android Debug Bridge","Apache 2.0","https://android.googlesource.com/platform/system/core/+/master/adb/","Android"),
                 new OpenSourceProject("Newtonsoft.Json","Json.NET is a popular high-performance JSON framework for .NET","MIT","https://www.newtonsoft.com/json","JamesNK"),
                 new OpenSourceProject("MaterialDesignInXAML","Google's Material Design in XAML & WPF, for C# & VB.Net. ","MIT","http://materialdesigninxaml.net","MaterialDesignInXAML"),
                 new OpenSourceProject("dpmpro","在 dpm 原有的基础上, 增加 remove-all-users 与 remove-all-accounts 功能. ","Apache 2.0","https://github.com/web1n/dpmpro","web1n"),
