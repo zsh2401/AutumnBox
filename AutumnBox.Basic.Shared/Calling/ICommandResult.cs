6729ca59c626caaa824acefdde507cc24e79fca5
zsh2401 <zsh2401@163.com>
2019-01-26 17:59:57 +0800

    加入ICommandResult接口

---
+++ AutumnBox.Basic.Shared/Calling/ICommandResult.cs
@@ -0,0 +1,19 @@
+﻿using AutumnBox.Basic.Data;
+
+namespace AutumnBox.Basic.Calling
+{
+    /// <summary>
+    /// 标准的命令结果格式
+    /// </summary>
+    public interface ICommandResult
+    {
+        /// <summary>
+        /// 返回码
+        /// </summary>
+        int ExitCode { get; }
+        /// <summary>
+        /// 输出内容
+        /// </summary>
+        Output Output { get; }
+    }
+}
