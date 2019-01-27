0d41aaf2496266794d5ea47cd2e5f4652a1935ad
zsh2401 <zsh2401@163.com>
2019-01-27 18:04:14 +0800

    正在设计的BasicCommandExecutor

--- AutumnBox.Basic.Shared/Calling/BasicCommandExecutor.cs
+++ AutumnBox.Basic.Shared/Calling/BasicCommandExecutor.cs
@@ -1,27 +1,63 @@
-﻿//using AutumnBox.Basic.Data;
-//using System.Diagnostics;
-//using System.Threading.Tasks;
+﻿using AutumnBox.Basic.Data;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
-//namespace AutumnBox.Basic.Calling
-//{
-//    class BasicCommandExecutor : INotifyOutput, IReceiveOutputByTo<BasicCommandExecutor>
-//    {
-//        public Task<ICommandResult> ExecuteAsync(string fileName, params string[] args)
-//        {
+namespace AutumnBox.Basic.Calling
+{
+    sealed class BasicCommandExecutor : INotifyOutput, IReceiveOutputByTo<BasicCommandExecutor>, IDisposable
+    {
+        public string PathEnv { get; set; }
+        private bool disposed = false;
+        private readonly object _executeLock = new object();
 
-//        }
-//        public ICommandResult Execute(string fileName, params string[] args)
-//        {
-//            var pInfo = new ProcessStartInfo()
-//            {
-//                RedirectStandardError = true,
-//                RedirectStandardOutput = true,
-//                RedirectStandardInput = false,
-//                UseShellExecute = false,
-//                CreateNoWindow = CreateNoWindow,
-//                FileName = fileName,
-//                Arguments = string.Join(" ", args),
-//            };
-//        }
-//    }
-//}
+        public event OutputReceivedEventHandler OutputReceived;
+
+        private Process CurrentProcess { get; set; }
+        public Task<ICommandResult> ExecuteAsync(string fileName, params string[] args)
+        {
+            ThrowIfDisposed();
+            return Task.Run(() =>
+            {
+                return Execute(fileName, args);
+            });
+        }
+        public ICommandResult Execute(string fileName, params string[] args)
+        {
+            ThrowIfDisposed();
+            var pInfo = new ProcessStartInfo()
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = fileName,
+                Arguments = string.Join(" ", args),
+            };
+            pInfo.EnvironmentVariables["path"] = $"{PathEnv};{pInfo.EnvironmentVariables["path"]}";
+            throw new NotImplementedException();
+        }
+        public void KillCurrent()
+        {
+            ThrowIfDisposed();
+        }
+        private void ThrowIfDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(nameof(BasicCommandExecutor));
+            }
+        }
+        public void Dispose()
+        {
+            disposed = true;
+            throw new NotImplementedException();
+        }
+
+        public BasicCommandExecutor To(Action<OutputReceivedEventArgs> callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutumnBox.Basic.Shared/Calling/ProcessHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AutumnBox.Basic.Calling
+{
+    /// <summary>
+    /// Calling命名空间中一些进程相关的通用辅助函数
+    /// </summary>
+    public static class ProcessHelper
+    {
+        public static ProcessStartInfo GetCmdStartInfo(string fileName, string args)
+        {
+            var pInfo = new ProcessStartInfo()
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                FileName = fileName,
+                Arguments = args
+            };
+            return pInfo;
+        }
+        public static void TaskKill(int pid) { }
+    }
+}
