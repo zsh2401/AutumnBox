fbf7af621c41b60b11286ecc1bbc60476076b421
zsh2401 <zsh2401@163.com>
2019-02-02 16:01:02 +0800

    公开LDoNotScan

--- AutumnBox.OpenFramework.Shared/Extension/LeafExtension/LDoNotScan.cs
+++ AutumnBox.OpenFramework.Shared/Extension/LeafExtension/LDoNotScan.cs
@@ -2,8 +2,11 @@
 
 namespace AutumnBox.OpenFramework.Extension.LeafExtension
 {
+    /// <summary>
+    /// 有此标记的函数将绝对不可能被扫描为主函数
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
-    class LDoNotScan : Attribute
+    public class LDoNotScan : Attribute
     {
     }
 }
