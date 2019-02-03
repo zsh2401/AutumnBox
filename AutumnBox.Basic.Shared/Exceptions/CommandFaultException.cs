0719c888dfa8f06f1ac9cde418c8ff8a5283a62f
zsh2401 <zsh2401@163.com>
2019-02-04 01:47:35 +0800

    添加一些异常处理

---
+++ AutumnBox.Basic.Shared/Exceptions/CommandFaultException.cs
@@ -0,0 +1,17 @@
+﻿using AutumnBox.Basic.Calling;
+using System;
+
+namespace AutumnBox.Basic.Exceptions
+{
+    class CommandFaultException : Exception
+    {
+        public CommandFaultException(ICommandResult commandResult,string command=null)
+        {
+            CommandResult = commandResult ?? throw new ArgumentNullException(nameof(commandResult));
+            Command = command;
+        }
+        public string Command { get; set; }
+
+        public ICommandResult CommandResult { get; }
+    }
+}
---
+++ AutumnBox.Basic.Shared/Util/CommandResultExtension.cs
@@ -0,0 +1,41 @@
+﻿using AutumnBox.Basic.Calling;
+using AutumnBox.Basic.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Basic.Util
+{
+    /// <summary>
+    /// 一些ICommandResult的拓展方法
+    /// </summary>
+    public static class CommandResultExtension
+    {
+        /// <summary>
+        /// 如果ExitCode不等于0,抛出异常
+        /// </summary>
+        /// <param name="result"></param>
+        /// <exception cref="CommandFaultException"></exception>
+        /// <returns></returns>
+        public static ICommandResult ThrowIfError(this ICommandResult result)
+        {
+            if (result.ExitCode != 0)
+            {
+                throw new CommandFaultException(result);
+            }
+            else
+            {
+                return result;
+            }
+        }
+        /// <summary>
+        /// 转化为一段可视字符串
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static string ToStdString(this ICommandResult result)
+        {
+            return $"exitcode:{result.ExitCode}{Environment.NewLine}{result.Output}";
+        }
+    }
+}
