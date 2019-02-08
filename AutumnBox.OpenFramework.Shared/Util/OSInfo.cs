c08e8fc95f2bf2c9404e770f94f25b57cc847c5c
zsh2401 <zsh2401@163.com>
2019-02-08 22:38:44 +0800

    一些类在Util中重新规划,删除IFrameworkManager

---
+++ AutumnBox.OpenFramework.Shared/Util/OSInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.OpenFramework.Util
+{
+    /// <summary>
+    /// 操作系统相关API
+    /// </summary>
+   public static class OSInfo
+    {
+    }
+}
---
+++ AutumnBox.OpenFramework.Shared/Util/Sound.cs
@@ -0,0 +1,34 @@
+﻿using AutumnBox.OpenFramework.Management;
+
+namespace AutumnBox.OpenFramework.Util
+{
+    /// <summary>
+    /// 秋之盒音效API
+    /// </summary>
+    public static class Sound
+    {
+        /// <summary>
+        /// 声音ID
+        /// </summary>
+        public enum Id
+        {
+            /// <summary>
+            /// OK
+            /// </summary>
+            Ok
+        }
+        /// <summary>
+        /// 播放音效
+        /// </summary>
+        /// <param name="id"></param>
+        public static void Play(Id id)
+        {
+            switch (id)
+            {
+                case Id.Ok:
+                    CallingBus.BaseApi.PlayOk();
+                    break;
+            }
+        }
+    }
+}
