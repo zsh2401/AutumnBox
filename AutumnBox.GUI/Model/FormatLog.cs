c4c74969427e881986a1b750eae685195a2f55a3
zsh2401 <zsh2401@163.com>
2019-02-11 02:41:12 +0800

    新版日志窗口!

--- AutumnBox.GUI/Model/FormatLog.cs
+++ AutumnBox.GUI/Model/FormatLog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Media;
 using AutumnBox.GUI.MVVM;
 using AutumnBox.Logging.Management;
 
@@ -9,6 +10,28 @@ namespace AutumnBox.GUI.Model
         private readonly ILog log;
 
         public DateTime Time => log.Time;
+
+        public string TimeString => Time.ToString("yy-MM-dd HH:mm:ss");
+        public string LogWindowTimeString => Time.ToString("HH:mm:ss");
+        public Color LevelColor
+        {
+            get
+            {
+                switch (Level)
+                {
+                    case "Debug":
+                        return Colors.Green;
+                    case "Info":
+                        return Color.FromRgb(34, 96, 0xDC);
+                    case "Exception":
+                        return Colors.Red;
+                    case "Warn":
+                        return Colors.Orange;
+                    default:
+                        return Colors.Gray;
+                }
+            }
+        }
 
         public string Level => log.Level;
 
@@ -21,7 +44,7 @@ namespace AutumnBox.GUI.Model
         {
             get
             {
-                return string.Format(FMT, DateTime.Now.ToString("yy-MM-dd HH:mm:ss"), log.Level, log.Category, log.Message);
+                return string.Format(FMT, TimeString, log.Level, log.Category, log.Message);
             }
         }
         public override string ToString()
--- AutumnBox.GUI/View/Windows/LogWindow.xaml.cs
+++ AutumnBox.GUI/View/Windows/LogWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using AutumnBox.GUI.Util.Debugging;
-using System;
+﻿using AutumnBox.GUI.Model;
+using AutumnBox.GUI.MVVM;
+using AutumnBox.GUI.ViewModel;
+using AutumnBox.Logging;
+using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AutumnBox.GUI.View.Windows
 {
@@ -13,33 +18,49 @@ namespace AutumnBox.GUI.View.Windows
         public LogWindow()
         {
             InitializeComponent();
-            TextBox.AppendText(LoggingStation.Instance.CurrentLogged);
-            TextBox.ScrollToEnd();
-            LoggingStation.Instance.Logging += Logger_Logged;
+            (DataContext as VMLog).Logs.CollectionChanged += Logs_CollectionChanged;
+
+            KeyDown += LogWindow_KeyDown;
         }
 
-        private void Logger_Logged(object sender, LogEventArgs e)
+        private void Logs_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-            App.Current.Dispatcher.Invoke(() =>
+            SViewer.ScrollToBottom();
+        }
+
+        private void LogWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyStates == Keyboard.GetKeyStates(Key.C) && Keyboard.Modifiers == ModifierKeys.Control)
             {
-                TextBox.AppendText(e.Content + Environment.NewLine);
-                TextBox.ScrollToEnd();
-            });
+                CopySelected();
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CopySelected()
         {
-            TextBox.ScrollToEnd();
+            SLogger<LogWindow>.Debug("Copy");
+            StringBuilder sb = new StringBuilder();
+            foreach (FormatLog log in LB.SelectedItems)
+            {
+                sb.Append(log);
+            }
+            Clipboard.SetText(sb.ToString());
         }
 
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            LoggingStation.Instance.Logging -= Logger_Logged;
-        }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void LB_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
-            Clipboard.SetText(TextBox.Text);
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+                {
+                    RoutedEvent = MouseWheelEvent,
+                    Source = sender
+                };
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
         }
     }
 }
