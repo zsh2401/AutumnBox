6023e05e1e782b66bd0fbf10a9c87218ea04afc1
zsh2401 <zsh2401@163.com>
2019-02-17 23:56:51 +0800

    加入用于连接秋之盒安卓端的程序集

---
+++ AutumnBox.Farewell.Shared/ConnectionConnnectedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Farewell
+{
+    public class ConnectionConnnectedEventArgs : EventArgs
+    {
+        public IConnection Connection { get; }
+        public ConnectionConnnectedEventArgs(IConnection connection)
+        {
+            this.Connection = connection;
+        }
+    }
+}
---
+++ AutumnBox.Farewell.Shared/IConnection.cs
@@ -0,0 +1,17 @@
+﻿using AutumnBox.Basic.Device;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Farewell
+{
+    public interface IConnection : IDisposable
+    {
+        IDevice Owner { get; }
+        void Connect();
+        object ExecuteCommand(string command);
+        bool IsAlive { get; }
+        event EventHandler<ConnectionConnnectedEventArgs> Connected;
+        event EventHandler<ConnectionDisposedEventArgs> Disposed;
+    }
+}
