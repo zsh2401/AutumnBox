778f908603d370ef7cb8ab07a8df9f5cf90e577f
zsh2401 <zsh2401@163.com>
2019-02-18 00:32:55 +0800

    SB Android Studio,build了半年,草

---
+++ AutumnBox.Farewell.Shared/Tools/ConnectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutumnBox.Farewell.Tools
+{
+    public static class ConnectionExtension
+    {
+        public static Version ClientVersion(this IConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutumnBox.Farewell.Shared/Tools/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.Farewell.Tools
+{
+    class Package
+    {
+        public string Name { get; set; }
+        public string PackageName { get; set; }
+        public string IsSystemApplication { get; set; }
+    }
+}
