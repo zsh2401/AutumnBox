10e0d4c2317c59ecc0d5deb6e35ac0bfcf8037fd
zsh2401 <zsh2401@163.com>
2019-02-19 04:02:11 +0800

    基本完成NullToVisibilityConverter

--- AutumnBox.GUI/Util/UI/Converters/BoolToVisibilityConverter.cs
+++ AutumnBox.GUI/Util/UI/Converters/BoolToVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace AutumnBox.GUI.Util.UI
+namespace AutumnBox.GUI.Util.UI.Converters
 {
     class BoolToVisibilityConverter : IValueConverter
     {
---
+++ AutumnBox.GUI/Util/UI/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿using AutumnBox.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AutumnBox.GUI.Util.UI.Converters
+{
+    [ValueConversion(typeof(object), typeof(Visibility), ParameterType = typeof(NullToVisibilityConverterParameter))]
+    class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SLogger.Info(this, $"p:{parameter} v:{value}");
+            if (parameter is NullToVisibilityConverterParameter.VisibleIf_IsNull)
+            {
+                return VisibleIf_IsNull(value);
+            }
+            else
+            {
+                return VisibleIf_IsNotNull(value);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null || parameter is NullToVisibilityConverterParameter.VisibleIf_IsNotNull)
+            {
+                return value == null ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return value != null ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        private Visibility VisibleIf_IsNull(object v)
+        {
+            return v == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private Visibility VisibleIf_IsNotNull(object v)
+        {
+            return v == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
