42635371e8fa6f864ed05432b1d795d64b911a5d
zsh2401 <zsh2401@163.com>
2019-02-19 03:39:20 +0800

    [中途存档]加入了新的Tip机制,尚未完成,目前准备大范围使用Converter机制

---
+++ AutumnBox.GUI/ViewModel/VMTip.cs
@@ -0,0 +1,34 @@
+﻿using AutumnBox.GUI.Model;
+using AutumnBox.GUI.MVVM;
+
+namespace AutumnBox.GUI.ViewModel
+{
+    class VMTip : ViewModelBase
+    {
+        public Tip Tip
+        {
+            get => _tip; set
+            {
+                _tip = value;
+                RaisePropertyChanged();
+                Handle();
+            }
+        }
+        private Tip _tip;
+
+        public object Content
+        {
+            get => Content; set
+            {
+                _content = value;
+                RaisePropertyChanged();
+            }
+        }
+        private object _content;
+
+        private void Handle()
+        {
+            Content = Tip.Content;
+        }
+    }
+}
