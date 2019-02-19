a20cfeb94968b31884d16aaa363db1b46858ec71
zsh2401 <zsh2401@163.com>
2019-02-20 04:06:10 +0800

    2019.2.20

--- AutumnBox.GUI/Properties/AssemblyInfo.cs
+++ AutumnBox.GUI/Properties/AssemblyInfo.cs
@@ -65,6 +65,6 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.2.12")]
-[assembly: AssemblyFileVersion("2019.2.12")]
+[assembly: AssemblyVersion("2019.2.20")]
+[assembly: AssemblyFileVersion("2019.2.20")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
