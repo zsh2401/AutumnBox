6e50d2550152c9a6712f1d25a31c3a6d4262cd03
zsh2401 <zsh2401@163.com>
2019-02-19 22:00:25 +0800

    CstApi基本完成设计与实现

---
+++ AutumnBox.GUI/Util/Net/Getters/CstGetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace AutumnBox.GUI.Util.Net.Getters
+{
+    public class CstGetter
+    {
+#if DEBUG
+        public string Url { get; set; } = "http://localhost:24010/_api_/cst/v1.xaml";
+#else
+        public string Url { get; set; } = App.Current.Resources["WebApiTips"].ToString();
+#endif
+        public Task<object> DoAsync(ParserContext context)
+        {
+            return Task.Run(() =>
+            {
+                return Do(context);
+            });
+        }
+        private object Do(ParserContext context)
+        {
+            var xamlStr = new WebClient().DownloadString(Url);
+            return App.Current.Dispatcher.Invoke(() =>
+            {
+                return XamlReader.Parse(xamlStr, context);
+            });
+        }
+    }
+}
