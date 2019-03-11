f673d1823504b793d4c283b97e2a723dcd5902cb
zsh2401 <zsh2401@163.com>
2019-03-11 15:11:36 +0800

    加入太极阴阳门激活支持

---
+++ AutumnBox.CoreModules/Extensions/Poweron/Dpm/ETaiChi.cs
@@ -0,0 +1,13 @@
+﻿using AutumnBox.OpenFramework.Extension;
+
+namespace AutumnBox.CoreModules.Extensions.Poweron.Dpm
+{
+    [ExtName("Act TaiChi", "zh-cn:一键激活太极阴阳门")]
+    [ExtIcon("Icons.taichi.png")]
+    class ETaiChi : DeviceOwnerSetter
+    {
+        protected override string ComponentName => "me.weishu.exp";
+
+        protected override string PackageName => "me.weishu.exp/.DeviceAdmin";
+    }
+}
