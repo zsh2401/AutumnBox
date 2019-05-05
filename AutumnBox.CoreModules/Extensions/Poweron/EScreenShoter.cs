0d8dbb9d276f1e0fef1b24a83db4fdce85b2baa7
zsh2401 <zsh2401@163.com>
2019-05-05 15:47:33 +0800

    修复截图器在特殊情况下的问题

--- AutumnBox.CoreModules/Extensions/Poweron/EScreenShoter.cs
+++ AutumnBox.CoreModules/Extensions/Poweron/EScreenShoter.cs
@@ -3,10 +3,13 @@
 ** date:  2018/8/13 9:37:22 (UTC +8:00)
 ** desc： ...
 *************************************************/
+using AutumnBox.Basic.Calling;
 using AutumnBox.Basic.Device;
-using AutumnBox.Basic.Device.Management.OS;
-using AutumnBox.Logging;
 using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.LeafExtension;
+using AutumnBox.OpenFramework.LeafExtension.Attributes;
+using AutumnBox.OpenFramework.LeafExtension.Fast;
+using AutumnBox.OpenFramework.LeafExtension.Kit;
 using System;
 using System.IO;
 using System.Windows.Forms;
@@ -16,14 +19,34 @@ namespace AutumnBox.CoreModules.Extensions
     [ExtName("Capture the screen and save it to pc", "zh-cn:截图并保存到电脑")]
     [ExtIcon("Icons.screenshot.png")]
     [ExtRequiredDeviceStates(DeviceState.Poweron)]
-    internal class EScreenShoter : OfficialVisualExtension
+    internal class EScreenShoter : LeafExtensionBase
     {
-        protected override int VisualMain()
+        [LMain]
+        public void EntryPoint(IDevice device, ILeafUI ui)
+        {
+            using (ui)
+            {
+                ui.Title = this.GetName();
+                ui.Icon = this.GetIconBytes();
+                ui.Show();
+                var executor = new CommandExecutor();
+                executor.OutputReceived += (s, e) => ui.WriteOutput(e.Text);
+                var tmpPath = GenerateTmpPath();
+                var capResult = executor.AdbShell(device, $"screencap -p {tmpPath}");
+                if (capResult.ExitCode == 0)
+                {
+                    executor.Adb(device, $"pull {tmpPath} \"{GetSaveTarget(ui)}\"");
+                    executor.AdbShell(device, $"rm -f {tmpPath}");
+                }
+                ui.Finish(capResult.ExitCode);
+            }
+        }
+        private FileInfo GetSaveTarget(ILeafUI ui)
         {
             DialogResult dialogResult = DialogResult.No;
             FileInfo path = null;
             string saveDir = null;
-            App.RunOnUIThread(() =>
+            ui.RunOnUIThread(() =>
             {
                 FolderBrowserDialog fbd = new FolderBrowserDialog();
                 dialogResult = fbd.ShowDialog();
@@ -33,28 +56,17 @@ namespace AutumnBox.CoreModules.Extensions
             {
                 string fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss");
                 path = new FileInfo(Path.Combine(saveDir, fileName + ".png"));
-                try
-                {
-                    var capture = GetDeviceCommander<ScreenCapture>();
-                    capture.To(OutputPrinter);
-                    capture.Capture();
-                    capture.SaveToPC(path);
-                    return OK;
-                }
-                catch (Exception ex)
-                {
-                    Logger.Warn("can't capture", ex);
-                    return ERR;
-                }
+                return path;
             }
             else
             {
-                return ERR_CANCELED_BY_USER;
+                return null;
             }
+
         }
-        protected override bool VisualStop()
+        private string GenerateTmpPath()
         {
-            return false;
+            return $"/sdcard/atmb_screenshot_{Guid.NewGuid().ToString()}.png";
         }
     }
 }
