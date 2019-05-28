c6be781b514aae9ebebf6e64473b99c8c0ceb1f7
zsh2401 <zsh2401@163.com>
2019-05-28 22:17:10 +0800

    更新版本号,使用AnimationPath

--- AutumnBox.GUI/Properties/AssemblyInfo.cs
+++ AutumnBox.GUI/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Windows;
     ResourceDictionaryLocation.None, //主题特定资源词典所处位置
     ResourceDictionaryLocation.SourceAssembly //常规资源词典所处位置
 )]
-[assembly: AssemblyVersion("2019.4.13")]
-[assembly: AssemblyFileVersion("2019.4.13")]
+[assembly: AssemblyVersion("2019.5.28")]
+[assembly: AssemblyFileVersion("2019.5.28")]
 [assembly: NeutralResourcesLanguage("zh-CN")]
