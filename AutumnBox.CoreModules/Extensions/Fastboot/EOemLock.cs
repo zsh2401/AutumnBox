ee4025199916938ec728122c630cbfd4b8c49b8a
zsh2401 <zsh2401@163.com>
2019-05-29 15:44:59 +0800

    oem lock器回归

--- AutumnBox.CoreModules/Extensions/Fastboot/EOemLock.cs
+++ AutumnBox.CoreModules/Extensions/Fastboot/EOemLock.cs
@@ -3,27 +3,37 @@
 ** date:  2018/9/12 16:01:08 (UTC +8:00)
 ** desc： ...
 *************************************************/
+using AutumnBox.Basic.Calling;
+using AutumnBox.Basic.Device;
 using AutumnBox.CoreModules.Aspect;
 using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.LeafExtension;
+using AutumnBox.OpenFramework.LeafExtension.Attributes;
+using AutumnBox.OpenFramework.LeafExtension.Kit;
+using AutumnBox.OpenFramework.Open;
 
 namespace AutumnBox.CoreModules.Extensions.Fastboot
 {
-    //[ExtName("加上BL锁", "en-us:Lock oem")]
-    //[ExtDesc("觉得解BL后不安全?想养老了?", "en-us:Do you wanna relock oem for your device?")]
-    //[ExtIcon("Icons.lock.png")]
-    //[ExtRequiredDeviceStates(Basic.Device.DeviceState.Fastboot)]
-    //[UserAgree("EOemLockWarn")]
-    //[UserAgree("EOemLockWarnAgain")]
-    //internal class EOemLock : OfficialVisualExtension
-    //{
-    //    protected override int VisualMain()
-    //    {
-    //        WriteInitInfo();
-    //        var result = CmdStation.GetFastbootCommand(TargetDevice, "oem lock")
-    //             .To(OutputPrinter)
-    //             .Execute();
-    //        WriteExitCode(result.ExitCode);
-    //        return result.ExitCode;
-    //    }
-    //}
+    [ExtName("加上BL锁", "en-us:Lock oem")]
+    [ExtDesc("觉得解BL后不安全?想养老了?", "en-us:Do you want to relock oem for your device?")]
+    [ExtIcon("Icons.lock.png")]
+    [ExtRequiredDeviceStates(DeviceState.Fastboot)]
+    [ExtText("warn", "This will erase all the data on your device, OK?", "zh-cn:此操作将可能清空你设备上的所有数据,确定吗?")]
+    [ExtText("warn2", "Once again, this will erase all the data on your device, OK?", "zh-cn:再问一次,此操作将 可 能 清空你设备上的所有数据,确定吗?")]
+    internal class EOemLock : LeafExtensionBase
+    {
+        [LMain]
+        public void EntryPoint(ILeafUI ui, IDevice device,TextAttrManager textManager)
+        {
+            using (ui)
+            {
+                if (!ui.DoYN(textManager["warn"])) return;
+                if (!ui.DoYN(textManager["warn2"])) return;
+                CommandExecutor executor = new CommandExecutor();
+                executor.OutputReceived += (s, e) => ui.WriteOutput(e.Text);
+                var exitCode = executor.Fastboot(device, "oem lock").ExitCode;
+                ui.Finish(exitCode);
+            }
+        }
+    }
 }
