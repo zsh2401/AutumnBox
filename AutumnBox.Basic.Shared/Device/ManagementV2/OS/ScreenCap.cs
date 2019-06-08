0ca21f4c517759cc12168823e1e66fbb83071c5b
zsh2401 <zsh2401@163.com>
2019-06-09 00:36:16 +0800

    加入ScreenCap类,截图模块使用此类,精简代码

---
+++ AutumnBox.Basic.Shared/Device/ManagementV2/OS/ScreenCap.cs
@@ -0,0 +1,51 @@
+﻿using AutumnBox.Basic.Calling;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutumnBox.Basic.Device.ManagementV2.OS
+{
+    /// <summary>
+    /// 基于screencap指令的截图器
+    /// </summary>
+    public sealed class ScreenCap
+    {
+        private readonly IDevice device;
+        private readonly ICommandExecutor executor;
+        private readonly string tmpDir;
+        /// <summary>
+        /// 构造一个截图器
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="executor"></param>
+        /// <param name="tmpDir"></param>
+        public ScreenCap(IDevice device, ICommandExecutor executor, string tmpDir = null)
+        {
+            this.device = device ?? throw new ArgumentNullException(nameof(device));
+            this.executor = executor ?? throw new ArgumentNullException(nameof(executor));
+            this.tmpDir = tmpDir ?? ".";
+        }
+        /// <summary>
+        /// 截图
+        /// </summary>
+        /// <returns>保存到PC上的截图文件名</returns>
+        public FileInfo Cap(bool wakeUpDevice = true)
+        {
+            if (wakeUpDevice)
+            {
+                new KeyInputer(device, executor).RaiseKeyEvent(AndroidKeyCode.WakeUp);
+            }
+            string fileName = GenerateUniqueFileName();
+            string pcFileName = Path.Combine(tmpDir, fileName);
+            executor.AdbShell(device, $"screencap -p /sdcard/{fileName}").ThrowIfError();
+            executor.Adb(device, $"pull /sdcard/{fileName} {pcFileName}").ThrowIfError();
+            executor.AdbShell(device, $"rm /sdcard/{fileName}");
+            return new FileInfo(pcFileName);
+        }
+        private static string GenerateUniqueFileName()
+        {
+            return $"atmb_screencap{Guid.NewGuid()}.png";
+        }
+    }
+}
--- AutumnBox.CoreModules/Extensions/Poweron/EScreenShoter.cs
+++ AutumnBox.CoreModules/Extensions/Poweron/EScreenShoter.cs
@@ -6,6 +6,7 @@
 using AutumnBox.Basic.Calling;
 using AutumnBox.Basic.Device;
 using AutumnBox.Basic.Device.ManagementV2.OS;
+using AutumnBox.Logging;
 using AutumnBox.OpenFramework.Extension;
 using AutumnBox.OpenFramework.LeafExtension;
 using AutumnBox.OpenFramework.LeafExtension.Attributes;
@@ -25,14 +26,18 @@ namespace AutumnBox.CoreModules.Extensions
     {
         [LMain]
         public void EntryPoint(IDevice device, ILeafUI ui, IStorageManager storageManager,
-            IAppManager app, Basic.Calling.ICommandExecutor executor)
+            IAppManager app, ICommandExecutor executor)
         {
             using (ui)
             {
                 //初始化LeafUI并展示
                 ui.Title = this.GetName();
                 ui.Icon = this.GetIconBytes();
-                executor.OutputReceived += (s, e) => ui.WriteOutput(e.Text);
+                executor.OutputReceived += (s, e) =>
+                {
+                    ui.WriteOutput(e.Text);
+                    SLogger<EScreenShoter>.Info(e.Text);
+                };
                 ui.Closing += (s, e) =>
                 {
                     executor.Dispose();
@@ -41,85 +46,34 @@ namespace AutumnBox.CoreModules.Extensions
 
                 ui.Show();
 
+                var screencap = new ScreenCap(device, executor, storageManager.CacheDirectory.FullName);
+                var file = screencap.Cap();
 
-                //生成一个在设备上的临时文件名
-                var tmpPath = GenerateTmpPath();
-                //点亮屏幕
-                new KeyInputer(device, executor).RaiseKeyEvent(AndroidKeyCode.WakeUp);
-                //进行截图,并保存到那个临时文件
-                var capResult = executor.AdbShell(device, $"screencap -p {tmpPath}");
-                //如果成功了
-                if (capResult.ExitCode == 0)
-                {
-                    //获取一个PC上的临时文件名
-                    var saveTarget = GetTempFile(storageManager);
-                    //拉取手机上的临时文件,并保存到PC
-                    executor.Adb(device, $"pull {tmpPath} \"{saveTarget}\"");
-                    //删除手机上的临时文件
-                    executor.AdbShell(device, $"rm -f {tmpPath}");
-                    //在UI线程运行
-                    ui.RunOnUIThread(() =>
-                    {
-                        //获取秋之盒主窗口
-                        var window = (Window)app.GetMainWindow();
-                        //构建一个HandyControl的图片浏览器
-                        var imgWindow = new ImageBrowser(saveTarget.FullName);
-                        //当主窗口关闭时,关闭图片浏览器窗口
-                        //不使用Window.Owner是为了避免图片窗始终在秋之盒主窗体上层
-                        window.Closing += (s, e) =>
-                        {
-                            imgWindow.Close();
-                        };
-                        //显示浏览器窗口
-                        imgWindow.Show();
-                    });
-                    //显示出来了,进度窗也没啥用了,直接关闭
-                    ui.EShutdown();
-                }
-                //有错误,进行错误显示
-                ui.Finish(capResult.ExitCode);
+                ui.WriteLine(file.FullName);
+                ShowOnUI(app, file.FullName);
+                //显示出来了,进度窗也没啥用了,直接关闭
+                //ui.Finish();
+                ui.Shutdown();
             }
         }
-        /// <summary>
-        /// 生成PC上的临时截图文件名
-        /// </summary>
-        /// <param name="storageManager"></param>
-        /// <returns></returns>
-        private FileInfo GetTempFile(IStorageManager storageManager)
+        private static void ShowOnUI(IAppManager app, string pngFile)
         {
-            string randomFileName = $"atmb_screenshot_{Guid.NewGuid().ToString()}.png";
-            var path = Path.Combine(storageManager.CacheDirectory.FullName, randomFileName);
-            return new FileInfo(path);
-        }
-        //private FileInfo GetSaveTarget(ILeafUI ui)
-        //{
-        //    DialogResult dialogResult = DialogResult.No;
-        //    FileInfo path = null;
-        //    string saveDir = null;
-        //    ui.RunOnUIThread(() =>
-        //    {
-        //        FolderBrowserDialog fbd = new FolderBrowserDialog();
-        //        dialogResult = fbd.ShowDialog();
-        //        saveDir = fbd.SelectedPath;
-        //    });
-        //    if (dialogResult == DialogResult.OK)
-        //    {
-        //        string fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        //        path = new FileInfo(Path.Combine(saveDir, fileName + ".png"));
-        //        return path;
-        //    }
-        //    else
-        //    {
-        //        return null;
-        //    }
-        //}
-        /// <summary>
-        /// 生成手机上的临时截图文件名
-        /// </summary>
-        /// <returns></returns>
-        private string GenerateTmpPath()
-        {
-            return $"/sdcard/atmb_screenshot_{Guid.NewGuid().ToString()}.png";
+            app.RunOnUIThread(() =>
+            {
+                //获取秋之盒主窗口
+                var window = (Window)app.GetMainWindow();
+                //构建一个HandyControl的图片浏览器
+                var imgWindow = new ImageBrowser(pngFile);
+                //当主窗口关闭时,关闭图片浏览器窗口
+                //不使用Window.Owner是为了避免图片窗始终在秋之盒主窗体上层
+                window.Closing += (s, e) =>
+                {
+                    imgWindow.Close();
+                };
+                //显示浏览器窗口
+                imgWindow.Show();
+            });
         }
     }
 }
+
