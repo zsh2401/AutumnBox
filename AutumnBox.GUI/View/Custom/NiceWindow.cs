e2065dcbd5513ced7a643713d6ff42cdbed61261
zsh2401 <zsh2401@163.com>
2019-08-17 17:17:02 +0800

    使用NiceWindow代替BlurWindow

---
+++ AutumnBox.GUI/View/Custom/NiceWindow.cs
@@ -0,0 +1,14 @@
+﻿using HandyControl.Controls;
+using System.Windows;
+
+namespace AutumnBox.GUI.View.Custom
+{
+    public class NiceWindow : System.Windows.Window
+    {
+        public NiceWindow()
+        {
+            //this.Style = this.FindResource("WindowBlur") as Style;
+            //this.ShowTitle = true;
+        }
+    }
+}
