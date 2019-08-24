535c9f5c3c2b21ecd0549fa6be4e2a3b9c84db8c
zsh2401 <zsh2401@163.com>
2019-08-24 14:05:36 +0800

    尝试搞独立进程的拓展模块模型

---
+++ AutumnBox.AECP.Shared/IClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.AECP
+{
+    public interface IClient
+    {
+        void Send();
+        string Receive();
+    }
+}
---
+++ AutumnBox.AECP.Shared/IServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.AECP
+{
+    public interface IServer
+    {
+        void Send();
+        string Receive();
+    }
+}
