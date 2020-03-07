eb5281fa3844a8f6ae80cb3b5ee1b87bd36efcef
zsh2401 <zsh2401@163.com>
2020-03-08 01:17:37 +0800

    初步定义ProxyKit

---
+++ AutumnBox.OpenFramework.Shared/Open/ProxyKit/IProxyCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.OpenFramework.Open.ProxyKit
+{
+    /// <summary>
+    /// 代理构建器
+    /// </summary>
+    public interface IProxyCreator
+    {
+        IProxy CreateProxyOf(Type type);
+    }
+}
---
+++ AutumnBox.OpenFramework.Shared/Open/ProxyKit/RequestingObjectEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AutumnBox.OpenFramework.Open.ProxyKit
+{
+    /// <summary>
+    /// 请求对象事件的处理委托
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    public delegate object RequestingObjectEventHandler(object sender, RequestingObjectEventArgs e);
+    /// <summary>
+    /// 请求对象事件的处理委托的参数
+    /// </summary>
+    public class RequestingObjectEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 构建
+        /// </summary>
+        /// <param name="type"></param>
+        public RequestingObjectEventArgs(Type type)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+        public Type Type { get; }
+    }
+}
