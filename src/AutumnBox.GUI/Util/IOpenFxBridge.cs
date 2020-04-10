b160aca59ad0e2dea9e1a73b9cd8736816eddbd5
zsh2401 <zsh2401@163.com>
2020-04-11 02:22:22 +0800

    进一步扩展用途

---
+++ src/AutumnBox.GUI/Util/IOpenFxBridge.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+* ==============================================================================
+*
+* Filename: IOpenFxBridge
+* Description: 
+*
+* Version: 1.0
+* Created: 2020/4/11 2:21:37
+* Compiler: Visual Studio 2019
+*
+* Author: zsh2401
+*
+* ==============================================================================
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutumnBox.GUI.Util
+{
+    interface IOpenFxBridge
+    {
+        void LoadOpenFx();
+    }
+}
