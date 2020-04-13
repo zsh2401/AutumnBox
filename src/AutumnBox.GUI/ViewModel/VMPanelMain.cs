041abdf1956c99e4b9673546e5a25af08c9fbfb3
zsh2401 <zsh2401@163.com>
2020-04-14 01:00:54 +0800

    新的Tab机制

--- src/AutumnBox.GUI/ViewModel/VMPanelMain.cs
+++ src/AutumnBox.GUI/ViewModel/VMPanelMain.cs
@@ -6,7 +6,14 @@
 using AutumnBox.GUI.MVVM;
 using AutumnBox.GUI.Services;
 using AutumnBox.Leafx.ObjectManagement;
+using AutumnBox.Logging;
+using HandyControl.Data;
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AutumnBox.GUI.ViewModel
 {
@@ -24,10 +31,55 @@ namespace AutumnBox.GUI.ViewModel
         private int tabSelectedIndex;
 
         [AutoInject]
+        private readonly ITabsManager tabsManager;
+
+        public ObservableCollection<ITabController> Tabs => tabsManager.Tabs;
+
+        [AutoInject]
         private readonly IAdbDevicesManager adbDevicesManager;
+
+        public ICommand ClosingTab
+        {
+            get
+            {
+                return _closingTab;
+            }
+            set
+            {
+                _closingTab = value;
+                RaisePropertyChanged();
+            }
+        }
+        private ICommand _closingTab;
+
+        public ICommand TabClosed
+        {
+            get
+            {
+                return _tabClosed;
+            }
+            set
+            {
+                _tabClosed = value;
+                RaisePropertyChanged();
+            }
+        }
+        private ICommand _tabClosed;
 
         public VMPanelMain()
         {
+            ClosingTab = new MVVMCommand(p =>
+            {
+                var e = (CancelRoutedEventArgs)p;
+                var tabController = (ITabController)e.OriginalSource;
+                e.Cancel = !tabController.OnClosing();
+            });
+            TabClosed = new MVVMCommand(p =>
+            {
+                var tabController = ((p as RoutedEventArgs).OriginalSource as ITabController);
+                tabController?.OnClosed();
+                SLogger<VMPanelMain>.Info($"There is {Tabs.Count()} tab");
+            });
             adbDevicesManager.DeviceSelectionChanged += Instance_SelectedDevice;
         }
 
