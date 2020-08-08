5b4858ef0988d6dc94c7259a775d5860dfae6755
zsh2401 <zsh2401@163.com>
2020-08-09 04:47:09 +0800

    使用LogEasy作为默认的调试窗口

---
+++ src/AutumnBox.GUI/Views/Windows/LogEasyWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AutumnBox.GUI.Services;
+using AutumnBox.GUI.Util;
+using AutumnBox.Logging.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutumnBox.GUI.Views.Windows
+{
+    /// <summary>
+    /// LogEasyWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LogEasyWindow : Window
+    {
+        public LogEasyWindow()
+        {
+            InitializeComponent();
+            var logMg = this.GetComponent<ILoggingManager>();
+            logMg.Logs.All((log) =>
+            {
+                logBox.AppendText(log.ToFormatedString() + "\n");
+                return true;
+            });
+            logBox.ScrollToEnd();
+            logMg.Logs.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach (ILog item in e.NewItems)
+                    {
+                        this.Dispatcher.Invoke(() => this.logBox.AppendText(item.ToFormatedString() + "\n"));
+                    }
+                    App.Current.Dispatcher.Invoke(() => logBox.ScrollToEnd());
+                }
+            };
+        }
+    }
+}
