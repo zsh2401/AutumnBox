617268b346b05238ed5e0a8d47f4d58233f0c4a6
zsh2401 <zsh2401@163.com>
2020-08-08 22:12:33 +0800

    使用更新的平台工具包

--- scripts/get_adb.ps1
+++ scripts/get_adb.ps1
@@ -1,5 +1,5 @@
 $ADBGitStore = "https://github.com/zsh2401/AutumnBox-AdbBinaries-Store"
-$BranchName = "1.0.40"
+$BranchName = "1.0.41"
 $ADBBinariesDirectoryPath = [System.IO.Path]::Combine($PSScriptRoot, "../adb_binary")
 $ADBBinariesDirectory = [System.IO.DirectoryInfo]::new($ADBBinariesDirectoryPath);
 if ($ADBBinariesDirectory.Exists) {
